23c7ec0aa8237b37bb1259193926e0c44e342a93
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-17 22:29:52 +0600

    FE: Implemented Landing Component

---
+++ emis_frontend/src/components/landing.jsx
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import * as bootstrap from 'bootstrap'
+import API_BASE_URL from '../config';
+import Logo64 from '../assets/logos/emis-64x64.png';
+import HomeComponent from './home';
+import ActivityComponent from './activity';
+import SettingsComponent from './settings';
+import ProfileComponent from './profile';
+import AcademicCalendar from './fullCalendar';
+import Test from '../test';
+import { logout, getRefreshToken, getUserRole } from '../auth.js';
+
+
+const LandingComponent = ({ user: response, onLogoutSuccess }) => {
+    // console.log(response);
+
+    const handleSignOut = async (e) => {
+        e.preventDefault();
+
+        // Get the refresh token from wherever you have stored it
+        const refreshToken = getRefreshToken();
+        try {
+            const response = await axios.post(`${API_BASE_URL}/logout/`, {
+                refresh_token: refreshToken,
+            });
+
+            // Handle successful logout
+            console.log(response.data);
+            console.log('Sign-out successful');
+
+            // Logout from frontend by removing access_token from local storage
+            logout();
+
+            // Call the onLogoutSuccess function passed as prop from App.jsx to change log state
+            onLogoutSuccess(response.data)
+        }
+        catch (error) {
+            // Handle sign-out error
+            console.error('Sign-out error', error);
+        }
+    };
+
+    const userRole = getUserRole();
+    const [activeComponent, setActiveComponent] = useState('home');
+
+    const componentController = (id) => {
+        setActiveComponent(id);
+    };
+
+
+    return (
+        <div className='text-light'>
+
+            <header>
+                <div className="px-3 py-2">
+                    <div className="container">
+                        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                            <a href="/" className="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-beige text-decoration-none">
+                                <img className="ms-2" src={Logo64} alt="EMIS Logo" height="64" />
+                                <h1 className='display-4 px-2 header-beige'>EMIS</h1>
+                            </a>
+
+                            <ul className="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+                                <li>
+                                    <a id="home" className={`btn nav-link text-beige ${activeComponent === 'home' ? 'active-beige' : 'active-beige-hidden'}`} onClick={() => componentController('home')}>
+                                        <i className="bi bi-house-door text-center fs-4 d-block"></i>
+                                        <span className='fw-lighter'>Home</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a id="actions" className={`btn nav-link text-beige ${activeComponent === 'actions' ? 'active-beige' : 'active-beige-hidden'}`} onClick={() => componentController('actions')}>
+                                        <i className="bi bi-grid text-center fs-4 d-block"></i>
+                                        <span className="fw-lighter">Activity</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a id="settings" className={`btn nav-link text-beige ${activeComponent === 'settings' ? 'active-beige' : 'active-beige-hidden'}`} onClick={() => componentController('settings')}>
+                                        <i className="bi bi-gear text-center fs-4 d-block"></i>
+                                        <span className="fw-lighter">Settings</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a id="profile" className={`btn nav-link text-beige ${activeComponent === 'profile' ? 'active-beige' : 'active-beige-hidden'}`} onClick={() => componentController('profile')}>
+                                        <i className="bi bi-person-circle text-center fs-4 d-block"></i>
+                                        <span className="fw-lighter">Profile</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a id="signout" className="btn nav-link text-beige red-warning" onClick={(e) => handleSignOut(e)}>
+                                        <i className="bi bi-box-arrow-right text-center fs-4 d-block"></i>
+                                        <span className="fw-lighter">Signout</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="container mb-3">
+                    <div className="d-flex px-2 py-2 flex-wrap justify-content-center">
+                        <form className="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
+                            <input type="search" className="form-control border-0" placeholder="Search..." aria-label="Search" />
+                        </form>
+
+                        <div className="text-end">
+                            <a id="home" className={`btn nav-link text-beige ${activeComponent === 'test' ? 'active-beige' : 'active-beige-hidden'}`} onClick={() => componentController('test')}>
+                                .
+                            </a>
+                            <button type="button" className="btn btn-warning text-black text-capitalize" style={{ cursor: 'none' }}>
+                                {['administrator', 'teacher', 'student', 'staff'].includes(userRole) ? userRole : 'None'}
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </header>
+
+            <section className="bg-light text-darkblue mt-5 py-4 rounded-top-5 min-vh-100">
+                {activeComponent === 'home' && <HomeComponent componentController={componentController} />}
+                {activeComponent === 'actions' && <ActivityComponent />}
+                {activeComponent === 'settings' && <SettingsComponent />}
+                {activeComponent === 'profile' && <ProfileComponent />}
+                {activeComponent === 'academic_calendar' && <AcademicCalendar />}
+                {activeComponent === 'test' && <Test />}
+            </section>
+
+        </div>
+
+    );
+}
+
+export default LandingComponent;
