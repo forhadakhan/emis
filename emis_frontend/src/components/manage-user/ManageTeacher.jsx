9bda468025a4bb5f90f7a0db2c5d03a064042329
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-17 22:48:54 +0600

    FE: Add ManageTeacher Component

---
+++ emis_frontend/src/components/manage-user/ManageTeacher.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const ManageTeacher = ({ setActiveComponent }) => {
+    return (
+        <div>
+            <a className="icon-link icon-link-hover" href="#" onClick={() => setActiveComponent('main')}>
+                <i className="bi bi-arrow-bar-left"></i> Back to activity panel
+            </a>
+
+            <h3 className="pb-2 border-bottom">
+                <div className="icon-square mt-4 text-beige bg-darkblue d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                    <i className="bi bi-person-plus-fill"></i>
+                </div>
+                Manage Teacher
+            </h3>
+
+        </div>
+    );
+};
+
+
+export default ManageTeacher;
+
+ 
