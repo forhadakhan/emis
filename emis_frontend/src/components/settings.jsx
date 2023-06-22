fa0b23184680624fd8b8c97947360d5d8b105c51
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-23 01:08:25 +0600

    FE: Updated settings.jsx

--- emis_frontend/src/components/settings.jsx
+++ emis_frontend/src/components/settings.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
+import StaffSettings from './settings/StaffSettings';
 
 
 const SettingsComponent = () => {
-  
 
-  return (
-    <div>
-      
-    </div>
-  );
+
+    return (
+        <div>
+            <h2 className="text-center h-4 pb-4 text-darkblue border-bottom border-darkblue">
+                <i className="bi bi-gear-fill"></i>
+            </h2>
+
+            <StaffSettings />
+        </div>
+    );
 };
 
 export default SettingsComponent;
