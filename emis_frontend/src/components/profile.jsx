14179014734e3d534080ff8a491c3f4b273806e9
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-23 01:08:58 +0600

    FE: Updated profile.jsx

--- emis_frontend/src/components/profile.jsx
+++ emis_frontend/src/components/profile.jsx
@@ -1,40 +1,18 @@
 import React from 'react';
-import { getUserData } from '../auth';
+import { getUserData, getProfileData } from '../auth';
+import ViewProfile from './profile/ViewProfile';
 
-const ProfileComponent = () => {
+const ProfileComponent = ({ componentController }) => {
 
     const user = getUserData();
-    console.log(user);
-
-    const formatValue = (value) => {
-        if (typeof value === 'boolean') {
-            return value ? 'Yes' : 'No';
-        } else {
-            return value.toString();
-        }
-    };
+    const profile = getProfileData();
 
     return (
         <div>
-            <h2 className="text-center h-4 pb-4 text-darkblue border-bottom border-darkblue">Profile</h2>
-            <div className="p-4 border rounded-3 bg-light mx-auto" style={{ width: '400px' }}>
-                <table className="table table-hover table-light">
-                    <thead>
-                        <tr>
-                            <th>Property</th>
-                            <th>Value</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {Object.entries(user).map(([property, value]) => (
-                            <tr key={property}>
-                                <td className="text-end">{property} : </td>
-                                <td className="text-left">{formatValue(value)}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            <h2 className="text-center h-4 pb-4 text-darkblue border-bottom border-darkblue"><i className="bi bi-person"></i></h2>
+            
+            <ViewProfile user={user} profile={profile} componentController={componentController} />
+
         </div>
     );
 };
