e6e011b8c630cff05af80ab01ee3d07d968d6519
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-23 00:55:54 +0600

    BE: administrator app - Implemented urls.py

---
+++ emis_backend/administrator/urls.py
@@ -0,0 +1,18 @@
+# administrator/urls.py
+
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import AdministratorViewSet, AdministratorUsersView, AdministratorPartialUpdate, AdministratorDeleteView, GetAdministratorView
+
+router = DefaultRouter()
+router.register(r'', AdministratorViewSet, basename='administrator')
+
+urlpatterns = [
+    path('users/', AdministratorUsersView.as_view(), name='administrator_users_view'),
+    path('profile/', GetAdministratorView.as_view(), name='get_administrator_view'),
+    path('update-partial/<int:pk>/', AdministratorPartialUpdate.as_view(), name='administrator_partial_update'),
+    path('delete/<int:pk>/', AdministratorDeleteView.as_view(), name='administrator_delete'),
+    path('', include(router.urls)),
+]
+
+
