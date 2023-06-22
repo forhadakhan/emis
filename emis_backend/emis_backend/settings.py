8eb2cd320e5862386fd3ea5ae07ef95c4dfbda53
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-23 00:43:30 +0600

    BE: Implemented settings for database

--- emis_backend/emis_backend/settings.py
+++ emis_backend/emis_backend/settings.py
@@ -136,6 +136,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
     Custom
 """""""""""""""""""""""""""""""""""""""""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
+    }
+}
 
 
 """""""""""""""""""""""""""""""""""""""""
