9f03b629d4c6181ee1b45e12c8010f4a8952f069
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-25 23:45:01 +0600

    BE: Add Institute model

--- emis_backend/academy/models.py
+++ emis_backend/academy/models.py
@@ -7,12 +7,26 @@ from django.db import models
 ##################### Designation:
 #####################   - independent 
 class Designation(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=64)
     
     def __str__(self):
         return self.name
 #####################################################################
 
+
+#####################################################################
+##################### Institute:
+#####################   - independent 
+class Institute(models.Model):
+    name = models.CharField(max_length=124)
+    acronym = models.CharField(max_length=16)
+    code = models.IntegerField()
+    about = models.TextField()
+    history = models.TextField()
+
+    def __str__(self):
+        return self.name
+#####################################################################
 
 
 
