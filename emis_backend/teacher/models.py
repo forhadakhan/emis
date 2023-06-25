56aab78b8711fba10fd510db7345f4a11b58ebf8
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-26 02:03:54 +0600

    BE: Add Designation model

--- emis_backend/teacher/models.py
+++ emis_backend/teacher/models.py
@@ -1,3 +1,14 @@
+# teacher/models.py
+
 from django.db import models
 
-# Create your models here.
+#####################################################################
+##################### Designation:
+#####################   - independent. 
+#####################   linked with: create_designations().  
+class Designation(models.Model):
+    name = models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.name
+#####################################################################
