346a59b1ecb2072e8a799760052e26137ee6ab57
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-26 05:21:24 +0600

    BE: Add all necessary teacher urls

---
+++ emis_backend/teacher/urls.py
@@ -0,0 +1,18 @@
+# teacher/urls.py
+
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import TeacherViewSet, TeacherUsersView, TeacherPartialUpdate, TeacherDeleteView, GetTeacherView
+
+router = DefaultRouter()
+router.register(r'', TeacherViewSet, basename='teacher')
+
+urlpatterns = [
+    path('users/', TeacherUsersView.as_view(), name='teacher_users_view'),
+    path('profile/', GetTeacherView.as_view(), name='get_teacher_view'),
+    path('update-partial/<int:pk>/', TeacherPartialUpdate.as_view(), name='teacher-partial-update'),
+    path('delete/<int:pk>/', TeacherDeleteView.as_view(), name='teacher-delete'),
+    path('', include(router.urls)),
+]
+
+
