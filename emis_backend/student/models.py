673b51c34b14ff05bdf19f109dd2a7614231a22e
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-26 05:42:08 +0600

    BE: Add Student model

--- emis_backend/student/models.py
+++ emis_backend/student/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+# student/models.py  
 
-# Create your models here.
+from django.db import models
+from authentication.models import User
+
+class Student(models.Model):
+    GENDER_CHOICES = (
+        ('U', 'Undefined'),
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    )
+    gender = models.CharField(default='U', choices=GENDER_CHOICES, max_length=1)
+    permanent_address = models.TextField(blank=True, null=True)
+    present_address = models.TextField(blank=True, null=True)
+    nid = models.CharField(default=None, max_length=20, unique=True)
+    photo_id = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    father_name = models.CharField(blank=True, null=True, max_length=255)
+    mother_name = models.CharField(blank=True, null=True, max_length=255)
+    guardian_name = models.CharField(blank=True, null=True, max_length=255)
+    guardian_relationship = models.CharField(blank=True, null=True, max_length=255)
+    guardian_phone = models.CharField(blank=True, null=True, max_length=255)
+    guardian_email = models.EmailField(blank=True, null=True, max_length=255)
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User, related_name='student_updated_by', on_delete=models.SET_NULL, blank=True, null=True)
+    added_by = models.ForeignKey(User, related_name='student_added_by', on_delete=models.SET_NULL, blank=True, null=True)
+    history = models.JSONField(blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+
