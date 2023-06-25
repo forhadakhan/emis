efc1eca72e33a7eb5f28c3fc247e786ee7cdcca7
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-26 04:11:03 +0600

    BE: Removed unnecessary imports

--- emis_backend/teacher/models.py
+++ emis_backend/teacher/models.py
@@ -2,11 +2,6 @@
 
 from django.db import models
 from authentication.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.db.models.signals import post_migrate
-from django.dispatch import receiver
-from teacher.models import Designation
 
 
 #####################################################################
