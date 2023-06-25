6267bd5b45da6c55d8ec50e9f6dd4a928a422b23
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-26 05:15:52 +0600

    BE: Add view - TeacherViewSet

--- emis_backend/teacher/views.py
+++ emis_backend/teacher/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+# teacher/views.py
 
-# Create your views here.
+import json
+from datetime import date
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse
+from rest_framework import status, generics, viewsets
+from rest_framework.mixins import DestroyModelMixin
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from authentication.models import User
+from authentication.serializers import UserSerializer
+from .models import Teacher
+from .serializers import TeacherSerializer
+from authentication.views import UserDeleteView
+from rest_framework.views import APIView
+
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+    def create(self, request, *args, **kwargs):
+        teacher_data = request.data.copy()
+        user_data = json.loads(teacher_data.pop('user')[0])
+        user_data["role"] = "teacher"
+        user_data["is_staff"] = True
+        user_serializer = UserSerializer(data=user_data)
+        user_serializer.is_valid(raise_exception=True)
+        user = user_serializer.save()
+
+        teacher_data['user'] = user.id
+
+        serializer = self.get_serializer(data=teacher_data)
+        serializer.is_valid(raise_exception=True)
+        Teacher = serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
