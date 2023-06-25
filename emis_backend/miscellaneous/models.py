a6955d96cd52260f72f46ac004c1e2f0b326dfa4
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-25 16:35:43 +0600

    BE: Add ContactMessage model

--- emis_backend/miscellaneous/models.py
+++ emis_backend/miscellaneous/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    is_answered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
