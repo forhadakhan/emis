9c3881164e51e8f75144dabe6e5d885deacec4bc
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-26 02:05:10 +0600

    BE: Add create_designations on post_migrate

--- emis_backend/teacher/models.py
+++ emis_backend/teacher/models.py
@@ -12,3 +12,40 @@ class Designation(models.Model):
     def __str__(self):
         return self.name
 #####################################################################
+
+
+
+
+
+
+#####################################################################
+##################### create_designations:
+#####################   - dependent on: Designation.
+@receiver(post_migrate)
+def create_designations(sender, **kwargs):
+    if kwargs.get('app').__name__ == 'academy.models':
+        designations_data = [
+            'Teaching Assistant',
+            'Instructor',
+            'Lecturer',
+            'Senior Lecturer',
+            'Principal Lecturer',
+            'Assistant Professor',
+            'Associate Professor',
+            'Professor',
+            'Distinguished Professor',
+            'Honorary Professor',
+            'Research Associate',
+            'Postdoctoral Fellow',
+            'Chair/Chairperson',
+            'Head of Department',
+            'Dean',
+            'Emeritus Professor',
+            'Visiting Professor',
+            'Adjunct Professor',
+            'Research Professor',
+        ]
+
+        for designation_name in designations_data:
+            Designation.objects.get_or_create(name=designation_name)
+#####################################################################
