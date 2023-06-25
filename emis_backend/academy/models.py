066ef911f3fcd1c921e2c78a8311a0e7292ed3e0
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-25 23:49:06 +0600

    BE: Add Department model

--- emis_backend/academy/models.py
+++ emis_backend/academy/models.py
@@ -29,6 +29,22 @@ class Institute(models.Model):
 #####################################################################
 
 
+#####################################################################
+##################### Department:
+#####################   - dependent on: Institute 
+class Department(models.Model):
+    name = models.CharField(max_length=124)
+    acronym = models.CharField(max_length=16)
+    code = models.IntegerField()
+    about = models.TextField()
+    history = models.TextField()
+    institute = models.ForeignKey(Institute, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+#####################################################################
+
+
 
 #####################################################################
 ##################### Model:
