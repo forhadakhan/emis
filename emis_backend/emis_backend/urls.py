b96060223067c5f09703126848035c6206cfda3e
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-26 06:02:22 +0600

    BE: Include namespace(s)

--- emis_backend/emis_backend/urls.py
+++ emis_backend/emis_backend/urls.py
@@ -4,14 +4,14 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authentication.urls')),
+    path('api/', include('authentication.urls', namespace='authentication')),
     path('api/', include('social_django.urls', namespace='social')),
-    path('api/administrator/', include('administrator.urls')),
-    path('api/staff/', include('staff.urls')),
-    path('api/teacher/', include('teacher.urls')),
-    path('api/student/', include('student.urls')),
-    path('api/file/', include('file_handler.urls')),
-    path('api/email/', include('email_handler.urls')),
-    path('api/', include('miscellaneous.urls')),
+    path('api/administrator/', include('administrator.urls', namespace='administrator')),
+    path('api/staff/', include('staff.urls', namespace='staff')),
+    path('api/teacher/', include('teacher.urls', namespace='teacher')),
+    path('api/student/', include('student.urls', namespace='student')),
+    path('api/file/', include('file_handler.urls', namespace='file_handler')),
+    path('api/email/', include('email_handler.urls', namespace='email_handler')),
+    path('api/', include('miscellaneous.urls', namespace='miscellaneous')),
 ]
 
