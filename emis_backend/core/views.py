b10f503e0b334361b4496822567336b8c85abf75
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-27 05:49:46 +0600

    BE: Add CustomContentTypesView

--- emis_backend/core/views.py
+++ emis_backend/core/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+# core/views.py 
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from core.models import PermissionGroup
+from django.contrib.auth.models import Permission
+from django.apps import apps
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
+from django.http import JsonResponse
+
+
+class PermissionGroupCreateView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        if request.user.role != 'administrator':
+            return Response({"success": False, "message": "Only administrators can create permission groups"})
+
+        # Assuming the request contains the necessary data for creating a permission group
+        group_name = request.data.get('group_name')
+        permission_ids = request.data.get('permissions', [])  # Assuming a list of permission IDs is provided
+
+        try:
+            permission_group = PermissionGroup.objects.create(name=group_name)
+
+            # Get the permissions based on the provided permission IDs
+            permissions = Permission.objects.filter(id__in=permission_ids)
+
+            # Add the permissions to the permission group
+            permission_group.permissions.set(permissions)
+
+            return Response({"success": True, "message": "Permission group created successfully"})
+        except Exception as e:
+            return Response({"success": False, "message": str(e)})
+
+
+class PermissionGroupListView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        permission_groups = PermissionGroup.objects.all()
+
+        group_data = []
+        for group in permission_groups:
+            group_data.append({
+                'id': group.id,
+                'name': group.name,
+                'permissions': [permission.codename for permission in group.permissions.all()]
+            })
+
+        return Response(group_data)
+
+
+class CustomContentTypesView(APIView):
+    def get(self, request):
+        custom_apps = settings.CUSTOM_APPS
+        content_types = ContentType.objects.filter(app_label__in=custom_apps)
+        data = {
+            'content_types': [
+                {
+                    'app_label': content_type.app_label,
+                    'model': content_type.model,
+                }
+                for content_type in content_types
+            ]
+        }
+        return JsonResponse(data)
+
