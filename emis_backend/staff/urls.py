90333c1370561c0036c0c26c84ee55279df27e0e
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-26 08:55:46 +0600

    BE: Add app_name

--- emis_backend/staff/urls.py
+++ emis_backend/staff/urls.py
@@ -1,19 +1,6 @@
 # staff/urls.py
 
-# from django.urls import include, path
-# from rest_framework.routers import DefaultRouter
-# from .views import StaffViewSet, staff_users_view, get_staff_view, StaffPartialUpdateView
-
-# router = DefaultRouter()
-# router.register(r'', StaffViewSet, basename='staff')
-
-# urlpatterns = [
-#     path('users/', staff_users_view, name='staff_users_view'),
-#     path('profile/', get_staff_view, name='get_staff_view'),
-#     path('update-partial/<int:id>/', StaffPartialUpdateView.as_view(), name='staff-partial-update'),
-#     path('', include(router.urls)),
-# ]
-
+app_name = 'staff'
 
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
