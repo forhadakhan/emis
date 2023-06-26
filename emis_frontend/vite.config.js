19fb94d3506962b6a4df6cd3ffe27288e6f6c21a
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-26 06:15:56 +0600

    -

--- emis_frontend/vite.config.js
+++ emis_frontend/vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite'
-import nodePolyfills from 'vite-plugin-node';
-import viteNode from 'vite-plugin-node';
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nodePolyfills, 
-    viteNode,
-    react(),
+    react()
   ],
 });
