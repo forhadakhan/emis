7fcfd3939970469a75f5e016436202176f6ebf3a
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-27 05:55:26 +0600

    BE: Add urls - 'permission-group-list'

--- emis_backend/core/urls.py
+++ emis_backend/core/urls.py
@@ -4,12 +4,13 @@ app_name = 'core'
 
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import CustomContentTypesView, PermissionGroupCreateView
+from .views import CustomContentTypesView, PermissionGroupCreateView, PermissionGroupListView
 
 
 urlpatterns = [
     path('custom-content-types/', CustomContentTypesView.as_view(), name='custom_content_types_view'),
     path('permission-group-create/', PermissionGroupCreateView.as_view(), name='permission_group_create_view'),
+    path('permission-group-list/', PermissionGroupListView.as_view(), name='permission_group_list_view'),
 ]
 
 
