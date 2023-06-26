085be046de3261624110738e60a750e46c411006
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-27 01:18:32 +0600

    BE: Add PermissionGroup model

--- emis_backend/core/models.py
+++ emis_backend/core/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+# core/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import Permission
+
+class PermissionGroup(models.Model):
+    name = models.CharField(max_length=255)
+    permissions = models.ManyToManyField(Permission, related_name='permission_groups')
+
+    def __str__(self):
+        return self.name
+
