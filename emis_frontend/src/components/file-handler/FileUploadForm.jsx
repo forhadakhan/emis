5ec15a94e53a217d0c4fc5a4d50eaf04319af209
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-26 06:11:56 +0600

    FE: Update - handled minor issues

--- emis_frontend/src/components/file-handler/FileUploadForm.jsx
+++ emis_frontend/src/components/file-handler/FileUploadForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import API_BASE_URL from '../../config';
+import API_BASE_URL from '../../utils/config';
 
 const FileUploadForm = ({ onFileUpload, accept_type = 'any', deactive=false }) => {
     const [selectedFile, setSelectedFile] = useState(null);
