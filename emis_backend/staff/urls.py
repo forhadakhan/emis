69101ad5ccd7aee3e025ceb1ed729880da1b78a2
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-27 01:58:50 +0600

    BE: Add 'permissions' and 'has-permission' urls

--- emis_backend/staff/urls.py
+++ emis_backend/staff/urls.py
@@ -4,7 +4,7 @@ app_name = 'staff'
 
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import StaffViewSet, StaffUsersView, StaffPartialUpdate, StaffDeleteView, GetStaffView
+from .views import StaffViewSet, StaffUsersView, StaffPartialUpdate, StaffDeleteView, GetStaffView, StaffAllPermissionsView, StaffHasPermissionView
 
 router = DefaultRouter()
 router.register(r'', StaffViewSet, basename='staff')
@@ -12,8 +12,10 @@ router.register(r'', StaffViewSet, basename='staff')
 urlpatterns = [
     path('users/', StaffUsersView.as_view(), name='staff_users_view'),
     path('profile/', GetStaffView.as_view(), name='get_staff_view'),
-    path('update-partial/<int:pk>/', StaffPartialUpdate.as_view(), name='staff-partial-update'),
-    path('delete/<int:pk>/', StaffDeleteView.as_view(), name='staff-delete'),
+    path('update-partial/<int:pk>/', StaffPartialUpdate.as_view(), name='staff_partial_update'),
+    path('delete/<int:pk>/', StaffDeleteView.as_view(), name='staff_delete'),
+    path('permissions/', StaffAllPermissionsView.as_view(), name='staff_permissions'),
+    path('has-permission/', StaffHasPermissionView.as_view(), name='staff_has_permission'),
     path('', include(router.urls)),
 ]
 
