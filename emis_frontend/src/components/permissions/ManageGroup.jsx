12ded49a7045fb697be91943e15b73d20453e7d7
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-28 03:16:08 +0600

    FE: Add ManageGroup to handle permission group related activity

---
+++ emis_frontend/src/components/permissions/ManageGroup.jsx
@@ -0,0 +1,59 @@
+/**
+ * Calling from: UserPermissions.jsx
+ * Calling To: CreatePermissionGroup.jsx;
+ */
+
+import React, { useState, useEffect } from 'react';
+import { getUserRole } from '../../utils/auth.js';
+import CreatePermissionGroup from './CreatePermissionGroup.jsx';
+
+const ManageGroup = ({ setPermissionComponent, breadcrumb }) => {
+    const [getComponent, setGetComponent] = useState('');
+    const updatedBreadcrumb = breadcrumb.concat(
+        <button className='btn p-0 m-0' onClick={() => setPermissionComponent('ManageGroup')}>
+            <i className="bi bi-people-fill"></i> Manage Group
+        </button>
+    );
+
+    const renderComponent = () => {
+        switch (getComponent) {
+            case 'CreatePermissionGroup':
+                return <CreatePermissionGroup setGetComponent={setPermissionComponent} />;
+            default:
+                return (
+                    <div className='m-5 text-center m-5 p-5'>
+                        Please select an option from above buttons. 
+                    </div>
+                );
+        }
+    };
+
+    return (
+        <div className="px-4 py-5">
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    {updatedBreadcrumb.map((item, index) => (
+                        <li className="breadcrumb-item" key={index}>{item}</li>
+                    ))}
+                </ol>
+            </nav>
+
+            <div className="container py-4">
+
+                <div class="btn-group" role="group" onClick={() => setGetComponent('CreatePermissionGroup')}>
+                    <button type="button" class="btn btn-dark" disabled={getComponent==='CreatePermissionGroup'}>
+                        <i className="bi bi-plus text-start me-2"></i>
+                        <span className="text-center">Create Group </span>
+                    </button>
+                </div>
+
+            </div>
+
+            <div>
+                {renderComponent()}
+            </div>
+        </div>
+    );
+};
+
+export default ManageGroup;
