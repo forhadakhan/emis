1c8730a4a7a9445993d9fb19947a90ae1d922d92
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-28 03:07:11 +0600

    .

--- emis_frontend/src/components/add-user/AddUser.jsx
+++ emis_frontend/src/components/add-user/AddUser.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {getUserRole} from '../../utils/auth.js'; 
 
-const AddUser = ({ setActiveComponent }) => {
+const AddUser = ({ setActiveComponent, filterComponents }) => {
     const user_role = getUserRole();
     return (
         <div className="px-4 py-5" id="add-users">
@@ -13,7 +13,7 @@ const AddUser = ({ setActiveComponent }) => {
             </h3>
 
             <div className="row row-cols-1 row-cols-md-2 row-cols-xl-4 py-4">
-                {user_role === "administrator" &&
+                {user_role === "administrator" && filterComponents('Add Admin') && (
                 <div className="col p-2 mx-auto">
                     <button id="add-admin" className="d-flex align-items-start bg-darkblue border rounded-3 p-1 w-100" onClick={() => setActiveComponent('AddAdministrator')} type='button'>
                         <div className="icon-rounded m-1 text-darkblue bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
@@ -23,7 +23,7 @@ const AddUser = ({ setActiveComponent }) => {
                             <h5 className="">Add Admin</h5>
                         </div>
                     </button>
-                </div>}
+                </div>)}
                 {user_role === "administrator" &&
                 <div className="col p-2 mx-auto">
                     <button id="add-staff" className="d-flex align-items-start bg-darkblue border rounded-3 p-1 w-100" onClick={() => setActiveComponent('AddStaff')} type='button'>
@@ -35,6 +35,7 @@ const AddUser = ({ setActiveComponent }) => {
                         </div>
                     </button>
                 </div>}
+                {(user_role === "administrator" || user_role === "staff") && 
                 <div className="col p-2 mx-auto">
                     <button id="add-teacher" className="d-flex align-items-start bg-darkblue border rounded-3 p-1 w-100" onClick={() => setActiveComponent('AddTeacher')} type='button'>
                         <div className="icon-rounded m-1 text-darkblue bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
@@ -44,7 +45,8 @@ const AddUser = ({ setActiveComponent }) => {
                             <h5 className="">Add Teacher</h5>
                         </div>
                     </button>
-                </div>
+                </div>}
+                {(user_role === "administrator" || user_role === "staff") && 
                 <div className="col  p-2 mx-auto">
                     <button id="add-student" className="d-flex align-items-start bg-darkblue border rounded-3 p-1 w-100" onClick={() => setActiveComponent('AddStudent')} type='button'>
                         <div className="icon-rounded m-1 text-darkblue bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
@@ -54,7 +56,7 @@ const AddUser = ({ setActiveComponent }) => {
                             <h5 className="">Add Student</h5>
                         </div>
                     </button>
-                </div>
+                </div>}
             </div>
         </div>
     );
