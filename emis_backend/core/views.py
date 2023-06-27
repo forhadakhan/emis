2ccbc60a6daaa179ca5276abd7b0066f66546938
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-27 06:20:46 +0600

    BE: reordered

--- emis_backend/core/views.py
+++ emis_backend/core/views.py
@@ -11,49 +11,6 @@ from django.contrib.contenttypes.models import ContentType
 from django.http import JsonResponse
 
 
-class PermissionGroupCreateView(APIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        if request.user.role != 'administrator':
-            return Response({"success": False, "message": "Only administrators can create permission groups"})
-
-        # Assuming the request contains the necessary data for creating a permission group
-        group_name = request.data.get('group_name')
-        permission_ids = request.data.get('permissions', [])  # Assuming a list of permission IDs is provided
-
-        try:
-            permission_group = PermissionGroup.objects.create(name=group_name)
-
-            # Get the permissions based on the provided permission IDs
-            permissions = Permission.objects.filter(id__in=permission_ids)
-
-            # Add the permissions to the permission group
-            permission_group.permissions.set(permissions)
-
-            return Response({"success": True, "message": "Permission group created successfully"})
-        except Exception as e:
-            return Response({"success": False, "message": str(e)})
-
-
-class PermissionGroupListView(APIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        permission_groups = PermissionGroup.objects.all()
-
-        group_data = []
-        for group in permission_groups:
-            group_data.append({
-                'id': group.id,
-                'name': group.name,
-                'permissions': [permission.codename for permission in group.permissions.all()]
-            })
-
-        return Response(group_data)
-
 
 class CustomContentTypesView(APIView):
     def get(self, request):
@@ -75,7 +32,6 @@ class CustomContentTypesView(APIView):
         }
 
         return JsonResponse(data)
-
 
 
 class ContentTypePermissionsView(APIView):
@@ -104,3 +60,50 @@ class ContentTypePermissionsView(APIView):
         }
 
         return JsonResponse(data)
+
+
+
+class PermissionGroupCreateView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        if request.user.role != 'administrator':
+            return Response({"success": False, "message": "Only administrators can create permission groups"})
+
+        # Assuming the request contains the necessary data for creating a permission group
+        group_name = request.data.get('group_name')
+        permission_ids = request.data.get('permissions', [])  # Assuming a list of permission IDs is provided
+
+        try:
+            permission_group = PermissionGroup.objects.create(name=group_name)
+
+            # Get the permissions based on the provided permission IDs
+            permissions = Permission.objects.filter(id__in=permission_ids)
+
+            # Add the permissions to the permission group
+            permission_group.permissions.set(permissions)
+
+            return Response({"success": True, "message": "Permission group created successfully"})
+        except Exception as e:
+            return Response({"success": False, "message": str(e)})
+
+
+
+class PermissionGroupListView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        permission_groups = PermissionGroup.objects.all()
+
+        group_data = []
+        for group in permission_groups:
+            group_data.append({
+                'id': group.id,
+                'name': group.name,
+                'permissions': [permission.codename for permission in group.permissions.all()]
+            })
+
+        return Response(group_data)
+
