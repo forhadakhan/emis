a71791a8529b9909a65d96b05af8680e1b3e052c
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-28 03:06:12 +0600

    FE: Updated Activities UI

--- emis_frontend/src/components/Activity.jsx
+++ emis_frontend/src/components/Activity.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {getUserRole} from '../utils/auth.js'; 
+import { getUserRole } from '../utils/auth.js';
 import AddUser from './add-user/AddUser';
 import AddAdministrator from './add-user/AddAdministrator';
 import AddStaff from './add-user/AddStaff';
@@ -11,18 +11,23 @@ import ManageStaff from './manage-user/manage-administrator-&-staff/ManageStaff'
 import ManageProfile from './manage-user/manage-administrator-&-staff/ManageProfile';
 import ManageTeacher from './manage-user/manage-teacher/ManageTeacher';
 import ManageStudent from './manage-user/manage-student/ManageStudent';
+import UserPermissions from './permissions/UserPermissions.jsx';
 
 
 const ActivityComponent = () => {
     const [activeComponent, setActiveComponent] = useState('main');
-    const [reference, setReference] = useState(null); 
+    const [reference, setReference] = useState(null);
     const [userType, setUserType] = useState(``);
     const user_role = getUserRole();
+
+    const breadcrumb = [
+        <button className='btn p-0 m-0' onClick={() => setActiveComponent('main')} ><i className="bi bi-grid-fill"></i> Activity </button>,
+    ]
 
     const renderComponent = () => {
         switch (activeComponent) {
             case 'main':
-                return <ActivityPanel setActiveComponent={setActiveComponent} />;
+                return <ActivityPanel setActiveComponent={setActiveComponent} breadcrumb={breadcrumb} />;
             case 'AddAdministrator':
                 return <AddAdministrator setActiveComponent={setActiveComponent} />;
             case 'AddStaff':
@@ -32,28 +37,43 @@ const ActivityComponent = () => {
             case 'AddStudent':
                 return <AddStudent setActiveComponent={setActiveComponent} />;
             case 'ManageAdministrator':
-                return <ManageAdministrator setActiveComponent={setActiveComponent} setReference={setReference}  setUserType={setUserType} />;
+                return <ManageAdministrator setActiveComponent={setActiveComponent} setReference={setReference} setUserType={setUserType} />;
             case 'ManageStaff':
-                return <ManageStaff setActiveComponent={setActiveComponent} setReference={setReference}  setUserType={setUserType} />;
+                return <ManageStaff setActiveComponent={setActiveComponent} setReference={setReference} setUserType={setUserType} />;
             case 'ManageTeacher':
                 return <ManageTeacher setActiveComponent={setActiveComponent} />;
             case 'ManageStudent':
                 return <ManageStudent setActiveComponent={setActiveComponent} />;
             case 'ManageProfile':
                 return <ManageProfile setActiveComponent={setActiveComponent} reference={reference} userType={userType} />;
+            case 'UserPermissions':
+                return <UserPermissions setActiveComponent={setActiveComponent} breadcrumb={breadcrumb} />;
             default:
                 return (
-                    <div>Looks like the rendering component in "activity" is having a wild party and forgot its job description!</div>
+                    <div className='m-5 text-center'>
+                        Looks like the rendering component in
+                        <button
+                            className="border-1 bg-transparent mx-2"
+                            onClick={() => setActiveComponent('main')}>
+                            Activity
+                        </button>
+                        is having a wild party and forgot its job description!!
+                    </div>
                 );
         }
     };
 
-    
+
+
     return (
         <div>
             <div className="container">
-                <h2 className="text-center h-4 pb-4 text-darkblue border-bottom border-darkblue">
-                    <button className="border-0 bg-transparent" onClick={() => setActiveComponent('main')}><i className="bi bi-grid-fill text-center fs-4 d-block"></i></button>
+                <h2 className="text-center h-4 text-darkblue">
+                    <button
+                        className="border-0 bg-transparent"
+                        onClick={() => setActiveComponent('main')}>
+                        <i className="bi bi-grid-fill text-center fs-4 d-block"></i>
+                    </button>
                 </h2>
                 {/* Render activeComponent */}
                 {renderComponent()}
@@ -62,15 +82,76 @@ const ActivityComponent = () => {
     );
 };
 
-const ActivityPanel = ({ setActiveComponent }) => {
+const ActivityPanel = ({ setActiveComponent, breadcrumb }) => {
+    const user_role = getUserRole();
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const elements = [
+        { id: 'add_admin', label: 'Add Admin', render: 'AddAdministrator', icon: 'bi-shield-fill-plus' },
+        { id: 'add_staff', label: 'Add Staff', render: 'AddStaff', icon: 'bi-shield-plus' },
+        { id: 'add_teacher', label: 'Add Teacher', render: 'AddTeacher', icon: 'bi-person-plus-fill' },
+        { id: 'add_student', label: 'Add Student', render: 'AddStudent', icon: 'bi-person-plus' },
+        { id: 'view_admin', label: 'Manage Admin', render: 'ManageAdministrator', icon: 'bi-shield-fill-plus' },
+        { id: 'view_staff', label: 'Manage Staff', render: 'ManageStaff', icon: 'bi-shield-plus' },
+        { id: 'view_teacher', label: 'Manage Teacher', render: 'ManageTeacher', icon: 'bi-person-plus-fill' },
+        { id: 'view_student', label: 'Manage Student', render: 'ManageStudent', icon: 'bi-person-plus' },
+        { id: 'view_permissions', label: 'User Permissions', render: 'UserPermissions', icon: 'bi-shield-shaded' },
+    ];
+
+    let allowedElements = [];
+
+    if (user_role === "administrator") {
+        allowedElements = [...elements];
+    } else if (user_role === "staff") {
+        allowedElements = [...elements];
+    }
+
+    // Filter the elements based on the search term
+    const filteredElements = allowedElements.filter(element => element.label.toLowerCase().includes(searchTerm.toLowerCase()));
+
     return (
-        <div>
-            <AddUser setActiveComponent={setActiveComponent} />
-            <ManageUser setActiveComponent={setActiveComponent} />
+        <div className="px-4 py-5" id="activities">
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    {breadcrumb.map((item, index) => (
+                        <li className="breadcrumb-item" key={index}>{item}</li>
+                    ))}
+                </ol>
+            </nav>
+            {/* Search input */}
+            <div className="mb-4">
+                <input
+                    type="text"
+                    className="form-control text-center border border-darkblue"
+                    placeholder="Find Activity"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </div>
+
+            <div className="row row-cols-1 row-cols-md-2 row-cols-xl-4 py-4">
+                {/* Render the filtered elements */}
+                {filteredElements.map(element => (
+                    <div className="col p-2 mx-auto" key={element.id}>
+                        <button
+                            id={element.id}
+                            className="d-flex align-items-start bg-darkblue border rounded-3 p-1 w-100"
+                            onClick={() => setActiveComponent(element.render)}
+                            type='button'
+                        >
+                            <div className="icon-rounded m-1 text-darkblue bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                                <i className={`bi ${element.icon}`}></i>
+                            </div>
+                            <div className="text-beige my-auto w-100 text-end pe-2">
+                                <h5 className="">{element.label}</h5>
+                            </div>
+                        </button>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
-
 
 
 
