b487917337d40c5080f4063f207764ce2302e7bf
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-29 00:52:26 +0600

    BE: Add url for 'permission-group-delete'

--- emis_backend/core/urls.py
+++ emis_backend/core/urls.py
@@ -1,16 +1,18 @@
-# administrator/urls.py
+# core/urls.py
 
 app_name = 'core'
 
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import CustomContentTypesView, PermissionGroupCreateView, PermissionGroupListView
+from .views import CustomContentTypesView, ContentTypePermissionsView, PermissionGroupCreateView, PermissionGroupListView, GroupDeleteView
 
 
 urlpatterns = [
     path('custom-content-types/', CustomContentTypesView.as_view(), name='custom_content_types_view'),
+    path('content-type-permissions/', ContentTypePermissionsView.as_view(), name='content_type_permissions_view'),
     path('permission-group-create/', PermissionGroupCreateView.as_view(), name='permission_group_create_view'),
     path('permission-group-list/', PermissionGroupListView.as_view(), name='permission_group_list_view'),
+    path('permission-group-delete/<int:group_id>/', GroupDeleteView.as_view(), name='permission_group_delete'),
 ]
 
 
