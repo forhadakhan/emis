d80946eafa4647bc51985d29a57b8dd7cd6c3481
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-01 00:03:57 +0600

    BE: Add url - 'contact-update/<int:id>/'

--- emis_backend/miscellaneous/urls.py
+++ emis_backend/miscellaneous/urls.py
@@ -3,8 +3,12 @@
 app_name = 'miscellaneous'
 
 from django.urls import path
-from .views import ContactView
+from .views import ContactMessageListView, ContactMessagePartialUpdateView, ContactView
+
 
 urlpatterns = [
+    path('contact-messages/', ContactMessageListView.as_view(), name='contact-message-list'),
+    path('contact-update/<int:id>/', ContactMessagePartialUpdateView.as_view(), name='contact-message-update'),
     path('contact/', ContactView.as_view(), name='contact'),
 ]
+
