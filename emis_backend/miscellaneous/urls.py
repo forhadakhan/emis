d1bb42d9ddcb4bbc68c9789472b657cc9af71d51
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-01 00:27:27 +0600

    BE: Add url - 'contact-message-delete/<int:id>/'

--- emis_backend/miscellaneous/urls.py
+++ emis_backend/miscellaneous/urls.py
@@ -3,12 +3,13 @@
 app_name = 'miscellaneous'
 
 from django.urls import path
-from .views import ContactMessageListView, ContactMessagePartialUpdateView, ContactView
+from .views import ContactMessageListView, ContactMessagePartialUpdateView, ContactMessageDeleteView, ContactView
 
 
 urlpatterns = [
     path('contact-messages/', ContactMessageListView.as_view(), name='contact-message-list'),
-    path('contact-update/<int:id>/', ContactMessagePartialUpdateView.as_view(), name='contact-message-update'),
+    path('contact-message-update/<int:id>/', ContactMessagePartialUpdateView.as_view(), name='contact-message-update'),
+    path('contact-message-delete/<int:id>/', ContactMessageDeleteView.as_view(), name='contact-message-delete'),
     path('contact/', ContactView.as_view(), name='contact'),
 ]
 
