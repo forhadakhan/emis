683361d69206c54ea0d5ad8cd2a56354f1a7f005
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-01 03:48:32 +0600

    BE: Add url - 'public-message-reply/'

--- emis_backend/email_handler/urls.py
+++ emis_backend/email_handler/urls.py
@@ -3,7 +3,7 @@
 app_name = 'email_handler'
 
 from django.urls import include, path
-from .views import EmailVerificationView, EmailVerificationConfirmView, ResendEmailVerificationView, PasswordResetView
+from .views import EmailVerificationView, EmailVerificationConfirmView, ResendEmailVerificationView, PasswordResetView, PublicMessageReplyEmailView
 
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     path('resend-verification/', ResendEmailVerificationView.as_view(), name='resend_email_verification'),
     path('verification/confirm/<str:uidb64>/<str:token>/', EmailVerificationConfirmView.as_view(), name='email_verification_confirm'),
     path('reset-password/', PasswordResetView.as_view(), name='reset_password_email'),
+    path('public-message-reply/', PublicMessageReplyEmailView.as_view(), name='public_message_reply_email'),
 ]
 
 
