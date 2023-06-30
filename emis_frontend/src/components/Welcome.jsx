e1333265876551d972785edb31200ee7d7a5a9f1
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-01 03:04:00 +0600

    FE: Update - minor bug fixed.

--- emis_frontend/src/components/Welcome.jsx
+++ emis_frontend/src/components/Welcome.jsx
@@ -112,150 +112,151 @@ const CoverParagraph = () => {
 
 const Contact = () => {
     const getInitialFormData = () => ({
-      name: '',
-      email: '',
-      message: ''
+        name: '',
+        email: '',
+        message: ''
     });
-  
+
     const [formData, setFormData] = useState(getInitialFormData);
     const [successMessage, setSuccessMessage] = useState(null);
     const [errorMessage, setErrorMessage] = useState(null);
-  
+
     const handleChange = (event) => {
-      setFormData({ ...formData, [event.target.name]: event.target.value });
+        setFormData({ ...formData, [event.target.name]: event.target.value });
     };
-  
+
     const handleSubmit = async (event) => {
-      event.preventDefault();
-  
-      try {
-        const response = await axios.post(`${API_BASE_URL}/contact/`, formData);
-  
-        if (response.status === 201) {
-          // Success message
-          setSuccessMessage(response.data.message);
-          setErrorMessage(null);
-          setFormData(getInitialFormData());
-        } else {
-          // Handle error response
-          setSuccessMessage(null);
-          setErrorMessage('An error occurred.');
+        event.preventDefault();
+
+        try {
+            const response = await axios.post(`${API_BASE_URL}/contact/`, formData);
+
+            if (response.status === 201) {
+                // Success message
+                setSuccessMessage(response.data.message);
+                handleClear();
+            } else {
+                // Handle error response
+                setSuccessMessage(null);
+                setErrorMessage('An error occurred.');
+            }
+        } catch (error) {
+            setSuccessMessage(null);
+            setErrorMessage('An error occurred.');
         }
-      } catch (error) {
-        setSuccessMessage(null);
-        setErrorMessage('An error occurred.');
-      }
     };
-  
+
+    const handleClearStatus = () => {
+        return  (formData.name === ''  && formData.email === '' && formData.message === '');
+      };
+      
     const handleClear = () => {
-      setFormData(getInitialFormData());
-      setSuccessMessage(null);
-      setErrorMessage(null);
+        setFormData(getInitialFormData());
     };
-  
+
     return (
-      <div className="p-5 my-5 text-beige">
-        <div className="">
-          <div className="row">
-            <div className="col-md-5 mr-auto">
-              <h2 className="mb-5">Contact Us</h2>
-              <ul className="list-unstyled pl-md-5 my-5">
-                <li className="d-flex mb-2">
-                  <span className="me-3"><i className="bi bi-pin-map-fill"></i></span> 42 Street Name, City Name Here, <br /> Union Countries
-                </li>
-                <li className="d-flex mb-2">
-                  <span className="me-3"><i className="bi bi-telephone-fill"></i></span> +8 (222) 345 6789
-                </li>
-                <li className="d-flex">
-                  <span className="me-3"><i className="bi bi-envelope-fill"></i></span>
-                  <a className="text-decoration-none text-beige" href="mailto:contact@emis.test">contact@emis.test</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-6">
-              <form className="mb-5" onSubmit={handleSubmit}>
+        <div className="p-5 my-5 text-beige">
+            <div className="">
                 <div className="row">
-                  <div className="col-md-12 form-group">
-                    <label htmlFor="name" className="col-form-label">Name</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      name="name"
-                      id="name"
-                      value={formData.name}
-                      onChange={handleChange}
-                      required
-                    />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-12 form-group">
-                    <label htmlFor="email" className="col-form-label">Email</label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      name="email"
-                      id="email"
-                      value={formData.email}
-                      onChange={handleChange}
-                      required
-                    />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-12 form-group">
-                    <label htmlFor="message" className="col-form-label">Message</label>
-                    <textarea
-                      className="form-control"
-                      name="message"
-                      id="message"
-                      cols="30"
-                      rows="7"
-                      value={formData.message}
-                      onChange={handleChange}
-                      required
-                    ></textarea>
-                  </div>
-                </div>
-  
-                <div className="row">
-                  <div className="col-md-12 my-3">
-                    {successMessage && (
-                      <div className="alert alert-success alert-dismissible fade show fw-bold" role="alert">
-                        <strong><i className="bi bi-check-circle-fill"></i></strong> {successMessage}
-                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                      </div>
-                    )}
-                    {errorMessage && (
-                      <div className="alert alert-warning alert-dismissible fade show fw-bold" role="alert">
-                        <strong><i className="bi bi-exclamation-triangle-fill"></i></strong> {errorMessage}
-                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                      </div>
-                    )}
-                  </div>
-                </div>
-  
-                <div className="row">
-                  <div className="col-md-12 d-flex">
-                    <div className='mx-auto'>
-                      <button className="btn btn-beige fw-bold m-3" type="submit">
-                        Send Message
-                      </button>
-                      <button className="btn btn-darkblue border boder-beige fw-bold m-3 pt-1" type="button" onClick={() => setTimeout(handleClear, 10)}>
-                        <i className="bi bi-x-lg"></i>
-                      </button>
+                    <div className="col-md-5 mr-auto">
+                        <h2 className="mb-5">Contact Us</h2>
+                        <ul className="list-unstyled pl-md-5 my-5">
+                            <li className="d-flex mb-2">
+                                <span className="me-3"><i className="bi bi-pin-map-fill"></i></span> 42 Street Name, City Name Here, <br /> Union Countries
+                            </li>
+                            <li className="d-flex mb-2">
+                                <span className="me-3"><i className="bi bi-telephone-fill"></i></span> +8 (222) 345 6789
+                            </li>
+                            <li className="d-flex">
+                                <span className="me-3"><i className="bi bi-envelope-fill"></i></span>
+                                <a className="text-decoration-none text-beige" href="mailto:contact@emis.test">contact@emis.test</a>
+                            </li>
+                        </ul>
                     </div>
-                  </div>
+                    <div className="col-md-6">
+                        <form className="mb-5" onSubmit={handleSubmit}>
+                            <div className="row">
+                                <div className="col-md-12 form-group">
+                                    <label htmlFor="name" className="col-form-label">Name</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        name="name"
+                                        id="name"
+                                        value={formData.name}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-12 form-group">
+                                    <label htmlFor="email" className="col-form-label">Email</label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        name="email"
+                                        id="email"
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-12 form-group">
+                                    <label htmlFor="message" className="col-form-label">Message</label>
+                                    <textarea
+                                        className="form-control"
+                                        name="message"
+                                        id="message"
+                                        cols="30"
+                                        rows="7"
+                                        value={formData.message}
+                                        onChange={handleChange}
+                                        required
+                                    ></textarea>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-md-12 my-3">
+                                    {successMessage && (
+                                        <div className="alert alert-success alert-dismissible fade show fw-bold" role="alert">
+                                            <strong><i className="bi bi-check-circle-fill"></i></strong> {successMessage}
+                                            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                        </div>
+                                    )}
+                                    {errorMessage && (
+                                        <div className="alert alert-warning alert-dismissible fade show fw-bold" role="alert">
+                                            <strong><i className="bi bi-exclamation-triangle-fill"></i></strong> {errorMessage}
+                                            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-md-12 d-flex">
+                                    <div className='mx-auto'>
+                                        <button className="btn btn-beige fw-bold m-3" type="submit">
+                                            Send Message
+                                        </button>
+                                        <button className="btn btn-darkblue border boder-beige fw-bold m-3 pt-1" disabled={handleClearStatus()} type="button" onClick={handleClear}>
+                                            <i className="bi bi-x-lg"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <div id="form-message-warning mt-4"></div>
+                    </div>
                 </div>
-              </form>
-              <div id="form-message-warning mt-4"></div>
             </div>
-          </div>
         </div>
-      </div>
     );
-  };
-  
+};
+
 
 
 
