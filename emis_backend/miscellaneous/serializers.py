da95b52775cb2c614c5fceffff7c77aa165a55fa
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-01 03:52:20 +0600

    BE: Update - reassigned fields to '__all__'

--- emis_backend/miscellaneous/serializers.py
+++ emis_backend/miscellaneous/serializers.py
@@ -1,7 +1,12 @@
+# miscellaneous/serializers.py 
+
+
 from rest_framework import serializers
 from .models import ContactMessage
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactMessage
-        fields = ['name', 'email', 'message']
+        fields = '__all__'
+
+
