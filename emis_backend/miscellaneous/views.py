1581f550d07d6c539197107b7493b299f988fb64
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-30 23:39:40 +0600

    BE: Add ContactMessageListView

--- emis_backend/miscellaneous/views.py
+++ emis_backend/miscellaneous/views.py
@@ -1,7 +1,12 @@
+# miscellaneous/views.py 
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from .serializers import ContactSerializer
+from .models import ContactMessage
+
 
 class ContactView(APIView):
     def post(self, request, format=None):
@@ -10,3 +15,11 @@ class ContactView(APIView):
             serializer.save()
             return Response({'message': "Your message was sent, thank you!"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ContactMessageListView(ListAPIView):
+    queryset = ContactMessage.objects.all()
+    serializer_class = ContactSerializer
+
+
