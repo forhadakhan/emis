ff0b2820d7e6a1d206fea9ac207d8d88ed41ea21
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-03 05:57:29 +0600

    BE: Add and Implement IsTeacher permission class

--- emis_backend/authentication/permissions.py
+++ emis_backend/authentication/permissions.py
@@ -46,6 +46,27 @@ class IsStaff(BasePermission):
 
 
 
+class IsTeacher(BasePermission):
+    def has_permission(self, request, view):
+        # Check if the user is authenticated
+        is_authenticated = IsAuthenticated().has_permission(request, view)
+
+        if is_authenticated and request.user.role:
+            # Check if the user's role is 'teacher'
+            return request.user.role == 'teacher'
+            
+        return False
+
+    def handle_permission_denied(self, request, message='Permission denied'):
+        if request.user.role:
+            role = request.user.role.capitalize()
+            error_message = f"{role} is unauthorized to make this request"
+            return Response({'detail': f"{message}. {error_message}"}, status=status.HTTP_403_FORBIDDEN)
+            
+        return Response({'detail': message}, status=status.HTTP_403_FORBIDDEN)
+
+
+
 
 
 
