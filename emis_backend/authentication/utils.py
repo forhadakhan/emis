b934609cf18a6d747faf070179217c673dc0e0b3
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-03 01:35:04 +0600

    BE: Add TokenDecoderToGetUserId utility class

--- emis_backend/authentication/utils.py
+++ emis_backend/authentication/utils.py
@@ -24,3 +24,35 @@ class TokenDecoder:
         except (ValueError, jwt.exceptions.DecodeError):
             return None
 
+
+######################################################
+### Takes: authorization_header 
+### Returns: user_id
+class TokenDecoderToGetUserId:
+    @staticmethod
+    def decode_token(authorization_header):
+
+        # Check if the Authorization header is present
+        if not authorization_header:
+            return Response({'message': 'Authorization/Access token missing'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Extract the access token from the Authorization header
+        try:
+            auth_type, access_token = authorization_header.split(' ')
+            if auth_type.lower() != 'bearer':
+                raise ValueError('Invalid Authorization header')
+        except ValueError:
+            return Response({'message': 'Invalid Authorization header'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Decode the access token to retrieve and return the user ID
+        try:
+            decoded_token = jwt.decode(
+                access_token, settings.SECRET_KEY, algorithms=['HS256'])
+            user_id = decoded_token['user_id']
+            return user_id
+        except jwt.exceptions.DecodeError:
+            return Response({'message': 'Invalid access token'}, status=status.HTTP_400_BAD_REQUEST)
+        except KeyError:
+            return Response({'message': 'Invalid access token'}, status=status.HTTP_400_BAD_REQUEST)
+
+
