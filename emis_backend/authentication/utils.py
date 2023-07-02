6d9202ab9c1ffc051c995fb49cb682923ee615e4
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-03 01:32:49 +0600

    BE: Add TokenDecoder utility class

--- emis_backend/authentication/utils.py
+++ emis_backend/authentication/utils.py
@@ -6,3 +6,21 @@ from rest_framework import status
 from rest_framework.response import Response
 from .models import User
 
+
+######################################################
+### Takes: authorization_header 
+### Returns: decoded_token
+class TokenDecoder:
+    @staticmethod
+    def decode_token(authorization_header):
+        try:
+            auth_type, access_token = authorization_header.split(' ')
+            if auth_type.lower() != 'bearer':
+                raise ValueError('Invalid Authorization header')
+
+            decoded_token = jwt.decode(
+                access_token, settings.SECRET_KEY, algorithms=['HS256'])
+            return decoded_token
+        except (ValueError, jwt.exceptions.DecodeError):
+            return None
+
