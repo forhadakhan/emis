75b5f881791c134b05542ed445e12fc97c3b3d68
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-03 06:05:59 +0600

    BE: Add url - 'get-user/'

--- emis_backend/authentication/urls.py
+++ emis_backend/authentication/urls.py
@@ -4,8 +4,21 @@ app_name = 'authentication'
 
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, LoginView, LogoutView, UserDeleteView, UserPartialUpdateView, ChangePasswordUserView, CheckPasswordView, ChangePasswordView, DeactivateUserView, ResetPasswordUserView 
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from .views import (
+    UserViewSet,
+    LoginView,
+    LogoutView,
+    UserDeleteView,
+    UserPartialUpdateView,
+    ChangePasswordUserView,
+    CheckPasswordView,
+    ChangePasswordView,
+    DeactivateUserView,
+    ResetPasswordUserView,
+    GetUserByUsernameView
+)
+
 
 router = DefaultRouter()
 router.register(r'', UserViewSet)
@@ -23,6 +36,7 @@ urlpatterns = [
     path('change-password/', ChangePasswordUserView.as_view(), name='change_password'),
     path('reset-password/', ResetPasswordUserView.as_view(), name='reset_password'),
     path('deactivate-me/', DeactivateUserView.as_view(), name='diactivate_me'),
+    path('get-user/', GetUserByUsernameView.as_view(), name='get_user'),
 ]
 
 
