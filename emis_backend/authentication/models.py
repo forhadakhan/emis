61d13f3d0397f53f68f8697170b620b52323bd68
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-06 06:02:31 +0600

    BE: Update User model - add email update action

--- emis_backend/authentication/models.py
+++ emis_backend/authentication/models.py
@@ -1,7 +1,7 @@
 # authentication/models.py
 
 from django.db import models
-from django.contrib.auth.models import AbstractUser 
+from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     ROLE_CHOICES = (
@@ -14,4 +14,10 @@ class User(AbstractUser):
     role = models.CharField(max_length=15, choices=ROLE_CHOICES)
     middle_name = models.CharField(max_length=255, blank=True, null=True)
     email_verified = models.BooleanField(default=False)
-    
+
+    def save(self, *args, **kwargs):
+        if self.pk:  # Check if the object is already in the database
+            original_email = User.objects.get(pk=self.pk).email
+            if self.email != original_email:  # Email field has changed
+                self.email_verified = False
+        super().save(*args, **kwargs)
