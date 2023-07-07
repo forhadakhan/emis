ae863a267d518147debf5fce87c2804d9a7f6693
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-07 20:27:15 +0600

    BE: add: include two new urls for term-choices to urlpatterns

--- emis_backend/academy/urls.py
+++ emis_backend/academy/urls.py
@@ -6,10 +6,13 @@ from django.urls import path
 
 from .views import (
     DesignationAPIView,
+    TermChoicesAPIView,  
 )
 
 
 urlpatterns = [
     path('designations/', DesignationAPIView.as_view(), name='designations'),
     path('designations/<int:pk>/', DesignationAPIView.as_view(), name='designation-pk'),
+    path('term-choices/', TermChoicesAPIView.as_view(), name='term-choices'),  
+    path('term-choices/<int:pk>/', TermChoicesAPIView.as_view(), name='term-choice-pk'),  
 ]
