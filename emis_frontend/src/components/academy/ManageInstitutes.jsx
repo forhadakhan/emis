82cd96a05de569fbcf4d345aa5b7f15799f37511
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-08 00:17:09 +0600

    FE: update: add 'list all terms' and 'add new term' button with logics in ManageInstitutes

--- emis_frontend/src/components/academy/ManageInstitutes.jsx
+++ emis_frontend/src/components/academy/ManageInstitutes.jsx
@@ -8,6 +8,7 @@ import React, { useEffect, useState } from 'react';
 
 
 const ManageInstitutes = ({ setActiveComponent, breadcrumb }) => {
+    const [showComponent, setShowComponent] = useState('TermList');
 
     const updatedBreadcrumb = breadcrumb.concat(
         <button className='btn p-0 m-0' onClick={() => setActiveComponent('ManageInstitutes')}>
@@ -31,6 +32,20 @@ const ManageInstitutes = ({ setActiveComponent, breadcrumb }) => {
 
             <h2 className="text-center m-5 px-2">Manage Institutes</h2>
 
+            <nav className="nav nav-pills flex-column flex-sm-row my-4">
+                <button
+                    className={`btn border flex-sm-fill text-center nav-link btn-beige m-1 p-2 fw-bold`}
+                    disabled={showComponent === 'TermList'}
+                    onClick={() => setShowComponent('TermList')}>
+                    List All Terms
+                </button>
+                <button
+                    className={`btn border flex-sm-fill text-center nav-link btn-beige m-1 p-2 fw-bold`}
+                    disabled={showComponent === 'AddTerm'}
+                    onClick={() => setShowComponent('AddTerm')}>
+                    Add New Term
+                </button>
+            </nav>
 
             <div className="">
                 
