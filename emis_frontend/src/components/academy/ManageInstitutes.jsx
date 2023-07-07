12265934af0ac0016fe76813f6edea0f048c86bd
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-08 01:07:39 +0600

    FE: feat: implement InstituteList component and update renderComponent() for InstituteList

--- emis_frontend/src/components/academy/ManageInstitutes.jsx
+++ emis_frontend/src/components/academy/ManageInstitutes.jsx
@@ -22,10 +22,12 @@ const ManageInstitutes = ({ setActiveComponent, breadcrumb }) => {
 
     const renderComponent = () => {
         switch (showComponent) {
+            case 'InstituteList':
+                return <InstituteList />;
             case 'AddInstitute':
                 return <AddInstitute />;
             default:
-                return '';
+                return <InstituteList />;
         }
     }
 
@@ -66,6 +68,148 @@ const ManageInstitutes = ({ setActiveComponent, breadcrumb }) => {
         </>
     );
 }
+
+
+
+const InstituteList = () => {
+    const [institutes, setInstitutes] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const [searchKeyword, setSearchKeyword] = useState('');
+    const [showEditModal, setShowEditModal] = useState(false);
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [selectedInstitute, setSelectedInstitute] = useState('');
+    const [refresh, setRefresh] = useState(false);
+    const accessToken = getAccessToken();
+
+    useEffect(() => {
+        const fetchInstitutes = async () => {
+            try {
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                        'Content-Type': 'application/json',
+                    },
+                };
+
+                const response = await axios.get(`${API_BASE_URL}/academy/institutes/`, config);
+                setInstitutes(response.data);
+                console.log(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchInstitutes();
+    }, [accessToken, refresh]);
+
+    useEffect(() => {
+        setFilteredData(institutes.filter(institute =>
+            institute.name.toLowerCase().includes(searchKeyword.toLowerCase())
+        ));
+    }, [institutes, searchKeyword]);
+
+    const handleSearch = (e) => {
+        setSearchKeyword(e.target.value);
+    };
+
+    const handleEditModal = (institute) => {
+        setSelectedInstitute(institute);
+        setShowEditModal(true);
+    };
+
+    const handleDeleteModal = (institute) => {
+        setSelectedInstitute(institute);
+        setShowDeleteModal(true);
+    };
+
+    const columns = [
+        {
+            name: 'Name',
+            selector: 'name',
+            sortable: true,
+        },
+        {
+            name: 'Acronym',
+            selector: 'acronym',
+            sortable: true,
+        },
+        {
+            name: 'Code',
+            selector: 'code',
+            sortable: true,
+        },
+        {
+            name: 'Action',
+            cell: (row) => (
+                <div className="mx-auto">
+                    <button
+                        type="button"
+                        className="btn border-0 btn-outline-primary p-1 mx-1"
+                        onClick={() => handleEditModal(row)}
+                    >
+                        <i className="bi bi-pen"></i>
+                    </button>
+                    <button
+                        type="button"
+                        className="btn border-0 btn-outline-danger p-1 mx-1"
+                        onClick={() => handleDeleteModal(row)}
+                    >
+                        <i className="bi bi-trash"></i>
+                    </button>
+                </div>
+            ),
+            button: true,
+        },
+    ];
+
+    const customStyles = {
+        rows: {
+            style: {
+                minHeight: '72px',
+                fontSize: '16px',
+            },
+        },
+        headCells: {
+            style: {
+                paddingLeft: '16px',
+                paddingRight: '8px',
+                fontSize: '19px',
+                backgroundColor: 'rgb(1, 1, 50)',
+                color: 'rgb(238, 212, 132)',
+                border: '1px solid rgb(238, 212, 132)',
+            },
+        },
+        cells: {
+            style: {
+                paddingLeft: '16px',
+                paddingRight: '8px',
+                fontWeight: 'bold',
+            },
+        },
+    };
+
+    return (
+        <div>
+            <div className="m-5">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    onChange={handleSearch}
+                    className="form-control text-center border border-darkblue"
+                />
+            </div>
+
+            <div className="rounded-top-4">
+                <DataTable
+                    columns={columns}
+                    data={filteredData}
+                    pagination
+                    customStyles={customStyles}
+                />
+            </div>
+        </div>
+    );
+};
 
 
 
