d8aa40500f00da83d96daaed4f744fe5dfe35f76
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-07 18:47:29 +0600

    FE: feat: implement TermList component and update renderComponent() for TermList

--- emis_frontend/src/components/academy/ManageTermChoices.jsx
+++ emis_frontend/src/components/academy/ManageTermChoices.jsx
@@ -5,6 +5,10 @@
 
 
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import DataTable from 'react-data-table-component';
+import API_BASE_URL from '../../utils/config.js';
+import { getAccessToken } from '../../utils/auth';
 
 
 const ManageTermChoices = ({ setActiveComponent, breadcrumb }) => {
@@ -18,10 +22,10 @@ const ManageTermChoices = ({ setActiveComponent, breadcrumb }) => {
 
     const renderComponent = () => {
         switch (showComponent) {
-            case '':
-                return "";
+            case 'TermList':
+                return <TermList />;
             default:
-                return "";
+                return <TermList />;
         }
     }
 
@@ -60,6 +64,147 @@ const ManageTermChoices = ({ setActiveComponent, breadcrumb }) => {
         </>
     );
 }
+
+
+
+const TermList = () => {
+    const [terms, setTerms] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const [selectedTerm, setSelectedTerm] = useState('');
+    const accessToken = getAccessToken();
+
+    useEffect(() => {
+        const fetchTerms = async () => {
+            try {
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                        'Content-Type': 'application/json',
+                    },
+                };
+
+                const response = await axios.get(`${API_BASE_URL}/academy/term-choices/`, config);
+                setTerms(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchTerms();
+    }, [accessToken]);
+
+    useEffect(() => {
+        setFilteredData(terms);
+    }, [terms]);
+
+    const handleSearch = (e) => {
+        const keyword = e.target.value.toLowerCase();
+        const filteredResults = terms.filter(
+            (term) =>
+                term.name.toLowerCase().includes(keyword) || 
+                term.start.toLowerCase().includes(keyword) || 
+                term.end.toLowerCase().includes(keyword) 
+        );
+        setFilteredData(filteredResults);
+    };
+
+    const handleEditModal = (term) => {
+        setSelectedTerm(term);
+        setShowEditModal(true);
+    };
+
+    const handleDeleteModal = (term) => {
+        setSelectedTerm(term);
+        setShowDeleteModal(true);
+    };
+
+    const columns = [
+        {
+            name: 'Name',
+            selector: 'name',
+            sortable: true,
+        },
+        {
+            name: 'Start',
+            selector: 'start',
+            sortable: true,
+        },
+        {
+            name: 'End',
+            selector: 'end',
+            sortable: true,
+        },
+        {
+            name: 'Action',
+            cell: (row) => (<>
+                <div className="mx-auto">
+                    <button
+                        type="button"
+                        className="btn border-0 btn-outline-primary p-1 mx-1"
+                        onClick={() => handleEditModal(row)}>
+                        <i className="bi bi-pen"></i>
+                    </button>
+                    <button
+                        type="button"
+                        className="btn border-0 btn-outline-danger p-1 mx-1"
+                        onClick={() => handleDeleteModal(row)}>
+                        <i className="bi bi-trash"></i>
+                    </button>
+                </div>
+            </>),
+            button: true
+        },
+    ];
+
+    
+    const customStyles = {
+        rows: {
+            style: {
+                minHeight: '72px',
+                fontSize: '16px',
+            },
+        },
+        headCells: {
+            style: {
+                paddingLeft: '16px',
+                paddingRight: '8px',
+                fontSize: '19px',
+                backgroundColor: 'rgb(1, 1, 50)',
+                color: 'rgb(238, 212, 132)',
+                border: '1px solid rgb(238, 212, 132)',
+            },
+        },
+        cells: {
+            style: {
+                paddingLeft: '16px',
+                paddingRight: '8px',
+                fontWeight: 'bold',
+            },
+        },
+    };
+
+    return (
+        <div>
+            <div className="m-5">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    onChange={handleSearch}
+                    className="form-control text-center border border-darkblue"
+                />
+            </div>
+
+            <div className="rounded-top-4">
+                <DataTable
+                    columns={columns}
+                    data={filteredData}
+                    pagination
+                    customStyles={customStyles}
+                />
+            </div>
+        </div>
+    );
+};
 
 
 
