6a4ddc5e0c533bf4f5c7bc309ea61864da768d97
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-09 17:11:24 +0600

    BE: update: include user data in the 'user' field of Teacher during serialization

--- emis_backend/teacher/serializers.py
+++ emis_backend/teacher/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Teacher
+from authentication.serializers import UserSerializer
+
 
 class TeacherSerializer(serializers.ModelSerializer):
+    user = UserSerializer()  # Use the UserSerializer for the user field
     class Meta:
         model = Teacher
         fields = "__all__" 
