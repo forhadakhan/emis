ea45dc25f20cc3713af84debc93d180d462e17f8
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-11 06:29:52 +0600

    FE: update: Rewrite ManageTeacher component as a link to teacher profile and enrollment CRUD

--- emis_frontend/src/components/manage-user/manage-teacher/ManageTeacher.jsx
+++ emis_frontend/src/components/manage-user/manage-teacher/ManageTeacher.jsx
@@ -1,18 +1,13 @@
 /**
  * Calling From: Activity.jsx 
+ * Calling to: TeacherController.jsx
  */
 
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import API_BASE_URL from '../../../utils/config';
-import { getAccessToken, getUserId } from '../../../utils/auth';
-import DataTableList from './DataTableList';
+import React from 'react';
+import TeacherController from './TeacherController';
 
 
-
-const ManageTeacher = ({ setActiveComponent, setReference, breadcrumb }) => {
-    const [teachers, setTeachers] = useState([]);
-    const accessToken = getAccessToken();
+const ManageTeacher = ({ setActiveComponent, breadcrumb, setReference }) => {
 
     const updatedBreadcrumb = breadcrumb.concat(
         <button className='btn p-0 m-0' onClick={() => setActiveComponent('ManageTeacher')}>
@@ -20,25 +15,9 @@ const ManageTeacher = ({ setActiveComponent, setReference, breadcrumb }) => {
         </button>
     );
 
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const response = await axios.get(`${API_BASE_URL}/teacher/users/`, {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    },
-                });
-                setTeachers(response.data);
-            } catch (error) {
-                console.error('Error fetching user data:', error);
-            }
-        };
 
-        fetchUserData();
-    }, []);
-
-    return (
-        <div>
+    return (<>
+        <div className="">
             <nav aria-label="breadcrumb">
                 <ol className="breadcrumb">
                     {updatedBreadcrumb.map((item, index) => (
@@ -46,22 +25,13 @@ const ManageTeacher = ({ setActiveComponent, setReference, breadcrumb }) => {
                     ))}
                 </ol>
             </nav>
-
-            {/* <a className="icon-link icon-link-hover" href="#" onClick={() => setActiveComponent('main')}>
-                <i className="bi bi-arrow-bar-left"></i> Back to activity panel
-            </a> */}
-
-            <h3 className="pb-2 border-bottom">
-                <div className="icon-square mt-4 text-beige bg-darkblue d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-                    <i className="bi bi-person-fill-gear"></i>
-                </div>
-                Manage Teacher
-            </h3>
-
-            <DataTableList source={teachers} setReference={setReference} setActiveComponent={setActiveComponent} />
-
         </div>
-    );
+
+        <h2 className="text-center m-5 px-2"><i className="bi bi-person-fill-gear"></i> Manage Teacher</h2>
+        
+        <TeacherController setActiveComponent={setActiveComponent} setReference={setReference} /> 
+
+    </>);
 };
 
 
