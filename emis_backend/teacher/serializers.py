f19948b86ffb0de51534cca4552f479b05ef291d
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-11 17:28:25 +0600

    BE: update: declare separate serializer for TeacherViewSet

--- emis_backend/teacher/serializers.py
+++ emis_backend/teacher/serializers.py
@@ -3,6 +3,11 @@ from .models import Teacher
 from authentication.serializers import UserSerializer
 
 
+class TeacherViewSetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = "__all__" 
+
 class TeacherSerializer(serializers.ModelSerializer):
     user = UserSerializer()  # Use the UserSerializer for the user field
     class Meta:
