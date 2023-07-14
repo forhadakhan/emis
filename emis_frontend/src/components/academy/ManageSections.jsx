e6c71710b95b408c3f0461d3d688b7b5f0089e33
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-15 02:51:23 +0600

    FE: feat: implement AddSection component and make some changes in ManageSections

--- emis_frontend/src/components/academy/ManageSections.jsx
+++ emis_frontend/src/components/academy/ManageSections.jsx
@@ -15,16 +15,16 @@ const ManageSections = ({ setActiveComponent, breadcrumb }) => {
     const accessToken = getAccessToken();
     const [showComponent, setShowComponent] = useState('SectionList');
     const [selectedSection, setSelectedSection] = useState('');
-    const [programs, setPrograms] = useState([]);
+    const [batches, setBatches] = useState([]);
     const [error, setError] = useState('');
 
     const updatedBreadcrumb = breadcrumb.concat(
         <button className='btn p-0 m-0' onClick={() => setActiveComponent('ManageSections')}>
-            <i className="bi-layers"></i> Manage Sections
+            <i className="bi-layers-half"></i> Manage Sections
         </button>
     );
 
-    // fetch programs for select 
+    // fetch batches for select 
     useEffect(() => {
         setError('');
         const fetchDepartments = async () => {
@@ -36,35 +36,35 @@ const ManageSections = ({ setActiveComponent, breadcrumb }) => {
                     },
                 };
 
-                const response = await axios.get(`${API_BASE_URL}/academy/programs/`, config);
-                setPrograms(response.data);
+                const response = await axios.get(`${API_BASE_URL}/academy/batches/`, config);
+                setBatches(response.data);
             } catch (error) {
-                setError(' Failed to fetch programs/batchs list.');
+                setError(' Failed to fetch batches list.');
                 console.error(error);
             }
         };
 
-        if (programs.length === 0) {
+        if (batches.length === 0) {
             fetchDepartments();
         }
 
     }, []);
 
-    const batchDetail = (batch) => {
-        setSelectedSection(batch);
+    const sectionDetail = (section) => {
+        setSelectedSection(section);
         setShowComponent('SectionDetails')
     }
 
     const renderComponent = () => {
         switch (showComponent) {
             case 'SectionList':
-                return <SectionList batchDetail={batchDetail} programs={programs} />;
+                return <SectionList batchDetail={sectionDetail} batches={batches} />;
             case 'AddSection':
-                return <AddSection programs={programs} />;
+                return <AddSection batches={batches} accessToken={accessToken} />;
             case 'SectionDetails':
-                return <SectionDetail viewSection={selectedSection} programs={programs} />;
+                return <SectionDetail viewSection={selectedSection} batches={batches} />;
             default:
-                return <SectionList batchDetail={batchDetail} programs={programs} />;
+                return <SectionList batchDetail={sectionDetail} batches={batches} />;
         }
     }
 
@@ -116,14 +116,149 @@ const ManageSections = ({ setActiveComponent, breadcrumb }) => {
 
 
 
+const AddSection = ({ accessToken, batches }) => {
+    const form = {
+        name: '',
+        batch: '',
+        max_seats: '',
+    }
+    const [formData, setFormData] = useState(form);
+    const [selectedBatch, setSelectedBatch] = useState('');
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
+
+
+    const handleInputChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const batchOptions = batches.map(batch => ({
+        value: batch.id,
+        label: `${batch.number} [Session: ${batch.session}]`
+    }));
+
+    const handleBatchChange = (selectedOption) => {
+        setSelectedBatch(selectedOption);
+        setFormData({ ...formData, batch: selectedOption.value });
+    };
+
+    const resetForm = () => {
+        setFormData(form);
+        setSelectedBatch('');
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            },
+        };
+
+        try {
+            const response = await axios.post(`${API_BASE_URL}/academy/sections/`, formData, config);
+            console.log('Section added:', response.data);
+            setMessage('Batch added successfully.');
+            setError('');
+            resetForm();
+        } catch (error) {
+            if (error.response && error.response.data) {
+                const errorMessages = Object.entries(error.response.data)
+                    .flatMap(([key, errorArray]) => {
+                        if (Array.isArray(errorArray)) {
+                            if (errorArray == "The fields name, batch must make a unique set.") {
+                                return "Probably this section already exists"
+                            }
+                            return errorArray.map(error => `[${key}] ${error}`);
+                        } else if (typeof errorArray === 'object') {
+                            const errorMessage = Object.values(errorArray).join(' ');
+                            return [`[${key}] ${errorMessage}`];
+                        } else {
+                            return [`[${key}] ${errorArray}`];
+                        }
+                    })
+                    .join('\n');
+
+                if (errorMessages) {
+                    setError(`Error creating section. \n${errorMessages}`);
+                } else {
+                    setError('Error creating section. Please try again.');
+                }
+            } else {
+                setError('Error creating section. Please try again.');
+            }
+            setMessage('');
+            console.error('Error creating section:', error);
+        }
+    };
+
+    return (
+        <div className="container mt-4">
+
+            {message && (
+                <div className={`alert alert-success alert-dismissible fade show mt-3 col-sm-12 col-md-6 mx-auto`} role="alert">
+                    <i className="bi bi-check-circle-fill"> </i>
+                    <strong> {message} </strong>
+                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={() => setMessage('')}></button>
+                </div>
+            )}
+            {error && (
+                <div className={`alert alert-danger alert-dismissible fade show mt-3 col-sm-12 col-md-6 mx-auto`} role="alert">
+                    <i className="bi bi-x-octagon-fill"> </i>
+                    <strong> {error} </strong>
+                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={() => setError('')}></button>
+                </div>
+            )}
+
+            <form onSubmit={handleSubmit}>
+                <div className="col-sm-12 col-md-8 my-2  mx-auto">
+                    <label className="text-secondary py-1">Batch: </label>
+                    <Select
+                        options={batchOptions}
+                        isMulti={false}
+                        value={selectedBatch}
+                        onChange={handleBatchChange}
+                    />
+                </div>
+                <div className="col-sm-12 col-md-8 my-2  mx-auto">
+                    <label className="text-secondary py-1" htmlFor="sectionName">Section Name</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="sectionName"
+                        name='name'
+                        value={formData.name}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <div className="col-sm-12 col-md-8 my-2  mx-auto">
+                    <label className="text-secondary py-1" htmlFor="maxSeats">Max Seats</label>
+                    <input
+                        type="number"
+                        className="form-control"
+                        id="maxSeats"
+                        name='max_seats'
+                        value={formData.max_seats}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <button type="submit" className="btn btn-darkblue2 mx-auto m-4 d-flex">Add Section</button>
+                <button type="button" className="btn btn-dark mx-auto m-4 btn-sm d-flex" onClick={resetForm}>Reset</button>
+            </form>
+        </div>
+    );
+};
+
+
+
+
 const SectionList = () => {
 
 }
 
-
-const AddSection = () => {
-
-}
 
 
 const SectionDetail = () => {
