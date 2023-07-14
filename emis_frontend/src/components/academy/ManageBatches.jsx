047b386dca92159a54996d6a0e0ea3b655838684
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-14 06:37:47 +0600

    FE: feat: implement BatchList to list all batches with detail button

--- emis_frontend/src/components/academy/ManageBatches.jsx
+++ emis_frontend/src/components/academy/ManageBatches.jsx
@@ -259,9 +259,149 @@ const AddBatch = ({ programs }) => {
 };
 
 
-const BatchList = () => {
+const BatchList = ({ batchDetail, programs }) => {
+    const accessToken = getAccessToken();
+    const [batches, setBatches] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const [error, setError] = useState('');
 
-}
+    useEffect(() => {
+        fetchBatches();
+    }, []);
+
+    useEffect(() => {
+        setFilteredData(batches);
+    }, [batches]);
+
+    const fetchBatches = async () => {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+            },
+        };
+        try {
+            const response = await axios.get(`${API_BASE_URL}/academy/batches/`, config);
+            setBatches(response.data);
+        } catch (error) {
+            setError(' Failed to fetch batch list.');
+            console.error('Error fetching batches:', error);
+        }
+    };
+
+    const getProgram = (id) => {
+        const program = programs.find((program) => program.id === id);
+        return program ? program.name : null;
+    };
+
+
+    const handleBatchClick = (batch) => {
+        batchDetail(batch);
+    };
+
+
+    const columns = [
+        {
+            name: 'Number',
+            selector: (row) => row.number,
+            sortable: true,
+        },
+        {
+            name: 'Session',
+            selector: (row) => row.session,
+            sortable: true,
+        },
+        {
+            name: 'Program',
+            selector: (row) => getProgram(row.program),
+            sortable: true,
+            width: '50%',
+        },
+        {
+            name: 'Actions',
+            button: true,
+            cell: (row) => (
+                <button
+                    type="button"
+                    className="btn btn-sm btn-outline-dark me-2 border-0"
+                    onClick={() => handleBatchClick(row)}
+                >
+                    Details
+                </button>
+            ),
+        },
+    ];
+
+    const customStyles = {
+        rows: {
+            style: {
+                minHeight: '72px', // override the row height
+                fontSize: '16px',
+            },
+        },
+        headCells: {
+            style: {
+                paddingLeft: '8px', // override the cell padding for head cells
+                paddingRight: '8px',
+                fontSize: '19px',
+                backgroundColor: 'rgb(1, 1, 50)',
+                color: 'rgb(238, 212, 132)',
+                border: '1px solid rgb(238, 212, 132)',
+            },
+        },
+        cells: {
+            style: {
+                paddingLeft: '8px', // override the cell padding for data cells
+                paddingRight: '8px',
+                fontWeight: 'bold'
+            },
+        },
+    };
+
+    const handleSearch = (e) => {
+        const keyword = e.target.value.toLowerCase();
+        const filteredResults = batches.filter(
+            (batch) =>
+                getProgram(batch.program).toLowerCase().includes(keyword) ||
+                batch.session.toLowerCase().includes(keyword) ||
+                batch.number.toString().toLowerCase().includes(keyword)
+        );
+        setFilteredData(filteredResults);
+    };
+
+    return (
+        <div>
+            {error && (
+                <div className={`alert alert-danger alert-dismissible fade show mt-3 col-sm-12 col-md-6 mx-auto`} role="alert">
+                    <i className="bi bi-x-octagon-fill"> </i>
+                    <strong> {error} </strong>
+                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={() => setError('')}></button>
+                </div>
+            )}
+
+            <div className="my-5 mx-md-5">
+                <input
+                    type="text"
+                    placeholder="Search with any field e.g. availability/duration ..."
+                    onChange={handleSearch}
+                    className="form-control text-center border border-darkblue"
+                />
+            </div>
+
+            <div className="rounded-4">
+                <DataTable
+                    columns={columns}
+                    data={filteredData}
+                    customStyles={customStyles}
+                    pagination
+                    paginationPerPage={10}
+                    paginationRowsPerPageOptions={[10, 20, 30]}
+                    highlightOnHover
+                />
+            </div>
+        </div>
+    );
+};
 
 const BatchDetail = () => {
 
