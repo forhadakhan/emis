a39576d92ffdf5ed744140751ccf02c74bf47514
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-16 02:44:09 +0600

    BE: chore: update string representation of Batch model

--- emis_backend/academy/models.py
+++ emis_backend/academy/models.py
@@ -163,7 +163,7 @@ class Batch(models.Model):
     status = models.BooleanField(default=True)
 
     def __str__(self):
-        return f'{self.department}: Batch {self.number}'
+        return f'{self.program}: Batch {self.number}'
 
     class Meta:
         unique_together = ['number', 'program']
