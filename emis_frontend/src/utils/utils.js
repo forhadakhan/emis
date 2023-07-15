6d63e5f4df20e5c60779607cc00f80bc7c1c33f4
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-15 17:02:57 +0600

    FE: feat: implement getOrdinal

--- emis_frontend/src/utils/utils.js
+++ emis_frontend/src/utils/utils.js
@@ -18,3 +18,25 @@ export const formatDateTime = (dateTimeString, timeZone = "Asia/Dhaka") => {
     const formatter = new Intl.DateTimeFormat(undefined, options);
     return formatter.format(dateTime);
 }
+
+
+export const getOrdinal = (number) => {
+    /**
+     * Take a number and return ordinal. for example: 
+     * input: 2     output: 2nd 
+     * input: 44     output: 44th
+     */
+    const parsedNumber = parseInt(number, 10);
+
+    if (isNaN(parsedNumber)) {
+        //   throw new Error('Input should be a valid number.');
+        return NaN;
+    }
+
+    const suffixes = ['th', 'st', 'nd', 'rd'];
+    const absNumber = Math.abs(parsedNumber);
+    const lastDigit = absNumber % 10;
+    const suffix = suffixes[lastDigit] || suffixes[0];
+
+    return parsedNumber + suffix;
+}
