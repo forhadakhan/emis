fc35d84322c774688b783c90e22de280d89defc5
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-17 19:10:28 +0600

    FE: refactor: rewrite ManageStudent with minimization

--- emis_frontend/src/components/manage-user/manage-student/ManageStudent.jsx
+++ emis_frontend/src/components/manage-user/manage-student/ManageStudent.jsx
@@ -1,17 +1,13 @@
 /**
  * Calling From: Activity.jsx 
+ * Calling to: StudentController.jsx
  */
 
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import API_BASE_URL from '../../../utils/config';
-import { getAccessToken, getUserId } from '../../../utils/auth';
-import DataTableList from './DataTableList';
+import React from 'react';
+import StudentController from './StudentController';
 
 
-const ManageStudent = ({ setActiveComponent, setReference, breadcrumb }) => {
-    const accessToken = getAccessToken();
-    const [students, setStudents] = useState([]);
+const ManageStudent = ({ setActiveComponent, breadcrumb, setReference }) => {
 
     const updatedBreadcrumb = breadcrumb.concat(
         <button className='btn p-0 m-0' onClick={() => setActiveComponent('ManageStudent')}>
@@ -19,26 +15,9 @@ const ManageStudent = ({ setActiveComponent, setReference, breadcrumb }) => {
         </button>
     );
 
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const response = await axios.get(`${API_BASE_URL}/student/users/`, {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    },
-                });
-                setStudents(response.data);
-            } catch (error) {
-                console.error('Error fetching user data:', error);
-            }
-        };
 
-        fetchUserData();
-    }, []);
-
-
-    return (
-        <div>
+    return (<>
+        <div className="">
             <nav aria-label="breadcrumb">
                 <ol className="breadcrumb">
                     {updatedBreadcrumb.map((item, index) => (
@@ -46,24 +25,15 @@ const ManageStudent = ({ setActiveComponent, setReference, breadcrumb }) => {
                     ))}
                 </ol>
             </nav>
-
-            {/* <a className="icon-link icon-link-hover" href="#" onClick={() => setActiveComponent('main')}>
-                <i className="bi bi-arrow-bar-left"></i> Back to activity panel
-            </a> */}
-
-            <h3 className="pb-2 border-bottom">
-                <div className="icon-square mt-4 text-beige bg-darkblue d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
-                    <i className="bi bi-person-gear"></i>
-                </div>
-                Manage Students
-            </h3>
-
-            <DataTableList source={students} setReference={setReference} setActiveComponent={setActiveComponent} />
-
         </div>
-    );
+
+        <h2 className="text-center m-5 px-2 font-merriweather"><i className="bi bi-person-gear"></i> Manage Student</h2>
+        
+        <StudentController setActiveComponent={setActiveComponent} setReference={setReference} /> 
+
+    </>);
 };
 
 
-
 export default ManageStudent;
+
