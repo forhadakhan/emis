c5bd5c6a24013efdb8ad78cfd13b836de975fc53
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-20 02:22:09 +0600

    FE: feat: implement CourseOfferList

--- emis_frontend/src/components/academy/ManageCourseOffer.jsx
+++ emis_frontend/src/components/academy/ManageCourseOffer.jsx
@@ -20,6 +20,7 @@ const ManageCourseOffer = ({ setActiveComponent, breadcrumb }) => {
     const [courses, setCourses] = useState([]);
     const [teachers, setTeachers] = useState([]);
 
+
     const updatedBreadcrumb = breadcrumb.concat(
         <button className='btn p-0 m-0' onClick={() => setActiveComponent('ManageCourseOffer')}>
             <i className="bi-file-medical-fill"></i> Manage Term Choices
@@ -123,7 +124,6 @@ const ManageCourseOffer = ({ setActiveComponent, breadcrumb }) => {
     };
 
 
-
     const handleBack = async () => {
         setShowComponent('CourseOfferList');
     };
@@ -186,9 +186,195 @@ const ManageCourseOffer = ({ setActiveComponent, breadcrumb }) => {
 
 
 
-const CourseOfferList = () => {
+const CourseOfferList = ({ teachers, semesters, courses, handleBack, courseDetail }) => {
+    const accessToken = getAccessToken();
+    const [courseOfferings, setCourseOfferings] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
+    const [error, setError] = useState('');
 
-}
+
+    useEffect(() => {
+        fetchCourseOfferings();
+    }, []);
+
+    useEffect(() => {
+        setFilteredData(courseOfferings);
+    }, [courseOfferings]);
+
+    const fetchCourseOfferings = async () => {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+            },
+        };
+        try {
+            const response = await axios.get(`${API_BASE_URL}/academy/course-offers/`, config);
+            setCourseOfferings(response.data);
+        } catch (error) {
+            setError(' Failed to fetch course offer list.');
+            console.error('Error fetching course offerings:', error);
+        }
+    };
+
+
+    const handleCourseClick = (course) => {
+        courseDetail(course);
+    };
+
+    const hasPrerequisites = (prerequisites) => {
+        return (prerequisites.length > 0) ? 'Yes' : 'No';
+    }
+
+    const getTeacherEnrollment = (data) => {
+        const teacherDesignations = data.teacher.designations.map(designation => designation.name).join(', ');
+        const teacherDepartments = data.teacher.departments.map(department => department.acronym).join(', ');
+        return `${teacherDesignations} (${teacherDepartments})`
+    }
+
+    const columns = [
+        {
+            name: 'Semester',
+            selector: (row) => `${row.semester.term.name} ${row.semester.year}`,
+            sortable: true,
+            cell: (row) => (
+                <div>
+                    <strong className='d-block'>{row.semester.term.name} {row.semester.year}</strong>
+                    <small>{row.semester.term.start} to {row.semester.term.end}</small>
+                </div>
+            ),
+            width: '15%',
+        },
+        {
+            name: 'Course',
+            selector: (row) => row.course.name,
+            sortable: true,
+            cell: (row) => (
+                <div>
+                    <strong className='d-block'>{row.course.name}</strong>
+                    <small>{row.course.acronym} {row.course.code}: Credit {row.course.credit}</small>
+                </div>
+            ),
+        },
+        {
+            name: 'Teacher',
+            selector: (row) => `${row.teacher.teacher.user.first_name} ${row.teacher.teacher.user.middle_name} ${row.teacher.teacher.user.last_name}  (${row.teacher.teacher.acronym}) `,
+            sortable: true,
+            cell: (row) => (
+                <div>
+                    <strong className='d-block'>{row.teacher.teacher.user.first_name} {row.teacher.teacher.user.middle_name} {row.teacher.teacher.user.last_name}  ({row.teacher.teacher.acronym})</strong>
+                    <small>{getTeacherEnrollment(row)}</small>
+                </div>
+            ),
+        },
+        {
+            name: 'Prerequisites',
+            selector: (row) => hasPrerequisites(row.course.prerequisites),
+            sortable: true,
+            width: '15%',
+        },
+        {
+            name: 'Actions',
+            button: true,
+            cell: (row) => (
+                <button
+                    type="button"
+                    className="btn btn-sm btn-outline-dark me-2 border-0"
+                    onClick={() => handleCourseClick(row)}
+                >
+                    Details
+                </button>
+            ),
+        },
+    ];
+
+    const customStyles = {
+        rows: {
+            style: {
+                minHeight: '72px', // override the row height
+                fontSize: '16px',
+            },
+        },
+        headCells: {
+            style: {
+                paddingLeft: '8px', // override the cell padding for head cells
+                paddingRight: '8px',
+                fontSize: '19px',
+                backgroundColor: 'rgb(1, 1, 50)',
+                color: 'rgb(238, 212, 132)',
+                border: '1px solid rgb(238, 212, 132)',
+            },
+        },
+        cells: {
+            style: {
+                paddingLeft: '8px', // override the cell padding for data cells
+                paddingRight: '8px',
+            },
+        },
+    };
+
+    const handleSearch = (e) => {
+        const keyword = e.target.value.toLowerCase();
+        const filteredResults = courseOfferings.filter(
+            (cf) =>
+                hasPrerequisites(cf.course.prerequisites).toLowerCase().includes(keyword) ||
+                `${cf.semester.term.name} ${cf.semester.year}`.toLowerCase().includes(keyword) ||
+                `${cf.teacher.teacher.user.first_name} ${cf.teacher.teacher.user.middle_name} ${cf.teacher.teacher.user.last_name} (${cf.teacher.teacher.acronym})`.toLowerCase().includes(keyword) ||
+                `${cf.semester.term.start} to ${cf.semester.term.end}`.toLowerCase().includes(keyword) ||
+                `${cf.course.acronym} ${cf.course.code}`.toLowerCase().includes(keyword) ||
+                `Credit ${cf.course.credit}`.toLowerCase().includes(keyword) ||
+                cf.course.name.toLowerCase().includes(keyword)
+        );
+        setFilteredData(filteredResults);
+    };
+
+
+    return (
+        <div>
+            {error && (
+                <div className={`alert alert-danger alert-dismissible fade show mt-3 col-sm-12 col-md-6 mx-auto`} role="alert">
+                    <i className="bi bi-x-octagon-fill"> </i>
+                    <strong> {error} </strong>
+                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={() => setError('')}></button>
+                </div>
+            )}
+
+            <div className="mb-3 me-5 input-group">
+                <label htmlFor="filter" className="d-flex me-2 ms-auto p-1">
+                    Filter:
+                </label>
+                <select id="filter" className="rounded bg-darkblue text-beige p-1" onChange={handleSearch}>
+                    <option value="">No Filter</option>
+                    <option value="Yes">Has Prerequisites</option>
+                    <option value="No">No Prerequisites</option>
+                    <option value="Lab">Lab Courses</option>
+                </select>
+            </div>
+
+            <div className="my-5 mx-md-5">
+                <input
+                    type="text"
+                    placeholder="Search ..."
+                    onChange={handleSearch}
+                    className="form-control text-center border border-darkblue"
+                />
+            </div>
+
+            <div className="rounded-4">
+                <DataTable
+                    columns={columns}
+                    data={filteredData}
+                    customStyles={customStyles}
+                    pagination
+                    paginationPerPage={10}
+                    paginationRowsPerPageOptions={[10, 20, 30]}
+                    highlightOnHover
+                />
+            </div>
+        </div>
+    );
+};
+
 
 
 const AddCourseOffer = ({ semesters, courses, teachers, getTeacher, handleBack }) => {
