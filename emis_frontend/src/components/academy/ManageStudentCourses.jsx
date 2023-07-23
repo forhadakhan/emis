eeb0da022e41ae686b385b1a2df4ac7e6a7c8246
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-23 22:14:23 +0600

    FE: chore: remove unnecessary code

--- emis_frontend/src/components/academy/ManageStudentCourses.jsx
+++ emis_frontend/src/components/academy/ManageStudentCourses.jsx
@@ -15,10 +15,7 @@ const ManageStudentCourses = ({ setActiveComponent, breadcrumb }) => {
     const accessToken = getAccessToken();
     const studentProfile = getProfileData();
     const studentId = studentProfile.id;
-    const enrollment = getEnrollmentData();
-    const semester = enrollment.semester;
     const [showComponent, setShowComponent] = useState('CourseList');
-    const [courses, setCourses] = useState([]);
     const [courseOffer, setCourseOffer] = useState('');
     const [enrolledCourses, setEnrolledCourses] = useState([]);
     const [error, setError] = useState('');
@@ -40,7 +37,6 @@ const ManageStudentCourses = ({ setActiveComponent, breadcrumb }) => {
         try {
             const response = await axios.get(`${API_BASE_URL}/academy/student/${studentId}/enrollments/`, config);
             setEnrolledCourses(response.data);
-            console.log(response.data);
         } catch (error) {
             if (error.response.status == 404) {
                 setError('No enrolled course(s) found for you. ');
@@ -55,55 +51,6 @@ const ManageStudentCourses = ({ setActiveComponent, breadcrumb }) => {
             fetchEnrolledCourses();
         }
     }, []);
-
-
-
-    // fetch courses for Prerequisites
-    useEffect(() => {
-        setError('');
-        const fetchCourses = async () => {
-            try {
-                const config = {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                        'Content-Type': 'application/json',
-                    },
-                };
-
-                const response = await axios.get(`${API_BASE_URL}/academy/courses/`, config);
-                const coursesById = {};
-                const data = response.data;
-                // Create a dictionary for faster lookup
-                data.forEach((course) => {
-                    coursesById[course.id] = course;
-                });
-                setCourses(coursesById);
-            } catch (error) {
-                setError('An error occurred while fetching courses list.');
-                console.error(error);
-            }
-        };
-
-        if (courses.length === 0) {
-            fetchCourses();
-        }
-
-    }, []);
-
-    const getPrerequisites = (ids) => {
-        const prerequisiteCourses = [];
-
-        // Iterate through the given ids and find prerequisite courses
-        ids.forEach((id) => {
-            const course = courses[id];
-            if (course) {
-                prerequisiteCourses.push(course);
-            }
-        });
-
-        return prerequisiteCourses;
-    };
-
 
 
     const handleBack = async () => {
@@ -121,7 +68,7 @@ const ManageStudentCourses = ({ setActiveComponent, breadcrumb }) => {
             case 'CourseList':
                 return <CourseList courseView={courseView} enrolledCourses={enrolledCourses} />
             case 'CourseDetails':
-                return <CourseDetails courseOffer={courseOffer} handleBack={handleBack} getPrerequisites={getPrerequisites} />
+                return <CourseDetails courseOffer={courseOffer} handleBack={handleBack} />
             default:
                 return <CourseList courseView={courseView} courseOfferings={enrolledCourses} />
         }
