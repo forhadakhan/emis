bdd86d7d17c13483fb3ca41809313493c68e7a08
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-25 04:46:55 +0600

    BE: feat: implement Comment model

--- emis_backend/comments/models.py
+++ emis_backend/comments/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+# comments/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from authentication.models import User
+from academy.models import CourseOffer 
+
+
+class Comment(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.created_at}"
+
