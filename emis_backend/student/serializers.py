14cd66cad5e5e875d706798d407d29586863c0fd
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-24 15:10:20 +0600

    FE: feat: implement StudentNestedSerializer with user details

--- emis_backend/student/serializers.py
+++ emis_backend/student/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Student
+from authentication.serializers import UserSerializer2
 
 class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = "__all__" 
+
+
+class StudentNestedSerializer(serializers.ModelSerializer):
+    user = UserSerializer2(read_only=True)
     class Meta:
         model = Student
         fields = "__all__" 
