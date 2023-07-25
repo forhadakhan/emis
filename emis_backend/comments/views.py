c0c9018ab87497dafdaeb8059227b425f411c8de
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-25 18:34:35 +0600

    BE: feat: write comment views

--- emis_backend/comments/views.py
+++ emis_backend/comments/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# comments/views.py
 
-# Create your views here.
+from rest_framework import generics
+from .models import Comment
+from .serializers import CommentSerializer
+
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
