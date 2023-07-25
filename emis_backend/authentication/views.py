aa53944bdb55cf054821d6d49dd8c4a942152213
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-25 13:54:09 +0600

    BE: refactor: reorder codes and remove redundant

--- emis_backend/authentication/views.py
+++ emis_backend/authentication/views.py
@@ -1,36 +1,31 @@
 # authentication/views.py
 
 import jwt
+from academy.views import TeacherEnrollmentAPIView
+from administrator.models import Administrator
+from administrator.serializers import AdministratorSerializer
+from authentication.models import User
 from authentication.permissions import IsAdministrator
-from django.core.exceptions import PermissionDenied
-from django.conf import settings
+from authentication.serializers import UserSerializer
+from authentication.utils import TokenDecoderToGetUserId
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import check_password
-from django.http import QueryDict
-from .serializers import UserSerializer
-from .models import User
-from .utils import TokenDecoderToGetUserRole
+from email_handler.views import EmailVerificationDirectView
 from rest_framework import status
-from rest_framework.authtoken.models import Token
-from rest_framework.exceptions import APIException
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
+# from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
 from rest_framework_simplejwt.tokens import RefreshToken
-from academy.views import TeacherEnrollmentAPIView
-from administrator.models import Administrator
-from administrator.serializers import AdministratorSerializer
 from staff.models import Staff
 from staff.serializers import StaffSerializer
 from teacher.models import Teacher
 from teacher.serializers import TeacherSerializer
 from student.models import Student
 from student.serializers import StudentSerializer
-from email_handler.views import EmailVerificationDirectView
 
 
 class UserViewSet(ModelViewSet):
@@ -260,28 +255,9 @@ class DeactivateUserView(APIView):
 
     def post(self, request):
         authorization_header = request.headers.get('Authorization')
-
-        # Check if the Authorization header is present
-        if not authorization_header:
-            return Response({'message': 'Access token missing'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Extract the access token from the Authorization header
-        try:
-            auth_type, access_token = authorization_header.split(' ')
-            if auth_type.lower() != 'bearer':
-                raise ValueError('Invalid Authorization header')
-        except ValueError:
-            return Response({'message': 'Invalid Authorization header'}, status=status.HTTP_400_BAD_REQUEST)
-
+        
         # Decode the access token to retrieve the user ID
-        try:
-            decoded_token = jwt.decode(
-                access_token, settings.SECRET_KEY, algorithms=['HS256'])
-            user_id = decoded_token['user_id']
-        except jwt.exceptions.DecodeError:
-            return Response({'message': 'Invalid access token'}, status=status.HTTP_400_BAD_REQUEST)
-        except KeyError:
-            return Response({'message': 'Invalid access token'}, status=status.HTTP_400_BAD_REQUEST)
+        user_id = TokenDecoderToGetUserId.decode_token(authorization_header)
 
         # Deactivate the user based on the retrieved user ID
         try:
