ae1f878daae1aea7b6a8bec893868da77a551a67
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-25 14:59:07 +0600

    BE: feat: implement CommentSerializer

---
+++ emis_backend/comments/serializers.py
@@ -0,0 +1,12 @@
+# comments/serializers.py 
+
+from rest_framework import serializers
+from comments.models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
