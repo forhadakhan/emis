f9fdcef8aa70271d9d39a639dd5f1a8e034cbd05
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-25 20:29:28 +0600

    BE: feat: implement CommentNestedSerializer

--- emis_backend/comments/serializers.py
+++ emis_backend/comments/serializers.py
@@ -1,10 +1,18 @@
 # comments/serializers.py 
 
+from authentication.serializers import UserBriefSerializer
 from rest_framework import serializers
 from comments.models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class CommentNestedSerializer(serializers.ModelSerializer):
+    user = UserBriefSerializer(read_only=True)
     class Meta:
         model = Comment
         fields = '__all__'
