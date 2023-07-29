6d019160db66685ebeedb8ad91196a26ebabdf35
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-29 22:30:16 +0600

    BE: feat: implement TeacherBriefSerializer

--- emis_backend/teacher/serializers.py
+++ emis_backend/teacher/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Teacher
-from authentication.serializers import UserSerializer
+from authentication.serializers import UserSerializer, UserBriefSerializer
 
 
 class TeacherViewSetSerializer(serializers.ModelSerializer):
@@ -8,8 +8,16 @@ class TeacherViewSetSerializer(serializers.ModelSerializer):
         model = Teacher
         fields = "__all__" 
 
+
 class TeacherSerializer(serializers.ModelSerializer):
-    user = UserSerializer()  # Use the UserSerializer for the user field
+    user = UserSerializer()  # Use the UserSerializer to serialize the user related fields
     class Meta:
         model = Teacher
         fields = "__all__" 
+
+
+class TeacherBriefSerializer(serializers.ModelSerializer):
+    user = UserBriefSerializer()  # Use the UserBriefSerializer to serialize the user related fields 
+    class Meta:
+        model = Teacher
+        fields = ['id', 'acronym', 'phone', 'photo_id', 'gender', 'user']
