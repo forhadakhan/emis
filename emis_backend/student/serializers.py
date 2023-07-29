060e3aa3b1382001acd6ba091633f6fe7f01249c
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-30 02:17:14 +0600

    BE: feat: implement StudentUsersSerializer

--- emis_backend/student/serializers.py
+++ emis_backend/student/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Student
-from authentication.serializers import UserSerializer2
+from authentication.serializers import UserSerializer2, UserBriefSerializer
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +13,13 @@ class StudentNestedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = "__all__" 
+
+
+
+class StudentUsersSerializer(serializers.ModelSerializer):
+    user = UserBriefSerializer()
+    class Meta:
+        model = Student
+        fields = "__all__" 
+
+
