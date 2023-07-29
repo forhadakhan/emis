0b5a4f306892777edec74c96e7b7a6bed6cbf1ab
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-30 02:09:47 +0600

    BE: add url to get a student by username(id)

--- emis_backend/student/urls.py
+++ emis_backend/student/urls.py
@@ -4,16 +4,25 @@ app_name = 'student'
 
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet, StudentUsersView, StudentPartialUpdate, StudentDeleteView, GetStudentView
+from .views import (
+    StudentViewSet, 
+    StudentUsersView, 
+    StudentPartialUpdate, 
+    StudentDeleteView, 
+    GetStudentView,
+    StudentRetrieveView,
+)
 
 router = DefaultRouter()
 router.register(r'', StudentViewSet, basename='student')
+
 
 urlpatterns = [
     path('users/', StudentUsersView.as_view(), name='student_users_view'),
     path('profile/', GetStudentView.as_view(), name='get_student_view'),
     path('update-partial/<int:pk>/', StudentPartialUpdate.as_view(), name='student_partial_update'),
     path('delete/<int:pk>/', StudentDeleteView.as_view(), name='student_delete'),
+    path('id/<str:user__username>/', StudentRetrieveView.as_view(), name='student-detail'),
     path('', include(router.urls)),
 ]
 
