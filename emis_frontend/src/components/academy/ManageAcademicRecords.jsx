456ada8c0f6fa7daa7243e71605f5ae1e1dd9bbd
FK <67508944+forhadakhan@users.noreply.github.com>
2023-07-30 03:56:21 +0600

    FE: feat: show basic info once a student is found

--- emis_frontend/src/components/academy/ManageAcademicRecords.jsx
+++ emis_frontend/src/components/academy/ManageAcademicRecords.jsx
@@ -182,7 +182,88 @@ const ListAllStudent = () => {
 
 
 // Sub Component 
-const StudentRecords = ({ studentData }) => {}
+const StudentRecords = ({ studentData }) => {
+    const enrollmentId = studentData.enrollment.id;
+    const [program, setProgram] = useState('')
+
+
+    // get enrollment and program details for student/teacher
+    useEffect(() => {
+        const fetchProgram = async () => {
+            try {
+                const accessToken = getAccessToken();
+                const programId = studentData.enrollment.batch_section.batch_data.program;
+
+                const response = await axios.get(
+                    `${API_BASE_URL}/academy/programs/${programId}/`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${accessToken}`,
+                        },
+                    }
+                );
+
+                setProgram(response.data);
+            } catch (error) {
+                // console.error('Error fetching program:', error);
+                setProgram('');
+            }
+        }
+        fetchProgram();
+    }, [enrollmentId])
+
+
+    return (<>
+
+        {/* if there student data, show some info about the student  */}
+        {studentData && <>
+            <div className="col-sm-12 col-md-8 mx-auto my-5">
+                <div className="row g-0">
+                    <div className="col-md-3">
+                        <div className={`rounded-2 mx-auto ${studentData.photo_id ? '' : 'bg-darkblue'} text-beige`} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth: '200px', height: '200px', }}>
+                            {studentData.photo_id ? (
+                                <img src={getFileLink(studentData.photo_id)} className="img-fluid rounded mx-auto d-flex border" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth: '200px', height: '200px', }} alt="..." />
+                            ) : (
+                                <i className="bi bi-person-bounding-box fs-1"></i>
+                            )}
+                        </div>
+                    </div>
+                    <div className="col-md-9 p-3">
+                        <div className="ms-5">
+                            {studentData.user && <>
+                                <h4 className="">{`${studentData.user.first_name} ${studentData.user.middle_name} ${studentData.user.last_name}`}</h4>
+                                <h5 className="fs-6 text-body-secondary"><i className="bi bi-person-fill"></i> {studentData.user.username}</h5>
+                                <h5 className="fs-6 text-body-secondary"><i className="bi bi-envelope-fill"></i> {studentData.user.email}</h5>
+                            </>}
+                            <h5 className="fs-6 text-body-secondary"><i className="bi bi-telephone-fill"></i> {studentData.phone}</h5>
+
+                            {/* Student Enrollment Information */}
+                            {program && <>
+                                <small className="d-block fw-bold text-body-secondary">{program.name}</small>
+                                <small className="d-block fw-bold text-body-secondary">
+                                    Batch: {getOrdinal(studentData.enrollment.batch_section.batch_data.number)};   
+                                    Section: {studentData.enrollment.batch_section.name}   
+                                </small>
+                                {/* semester  */}
+                                {/* <small className="d-block fw-bold text-body-secondary">{studentData.enrollment.semester.term.name} {studentData.enrollment.semester.year}</small> */}
+                            </>}
+
+                            {/* {enrollmentId && <>
+                                {studentData.enrollment.enrolled_by &&
+                                    <small className='d-block text-secondary'>Enrolled by: {studentData.enrollment.enrolled_by.username}</small>}
+                                {studentData.enrollment.updated_by &&
+                                    <small className='d-block text-secondary'>Last updated by: {studentData.enrollment.updated_by.username}</small>}
+                            </>} */}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>}
+
+
+    </>);
+}
 
 
 
