d919ce55532dd67d9cd0a49e99d2b63acae426ed
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-01 20:38:54 +0600

    FE: feat: implement getCurrentDateTimeInSingleStr()

--- emis_frontend/src/utils/utils.js
+++ emis_frontend/src/utils/utils.js
@@ -67,3 +67,17 @@ export const customDateFormat = (dateString) => {
       return `${day} ${month} ${year}, ${time}`;
 }
   
+
+export const getCurrentDateTimeInSingleStr = () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+  
+    return `${year}${month}${day}-${hours}${minutes}${seconds}`;
+    // 20230801-203557
+  }
+
