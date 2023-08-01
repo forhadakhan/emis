caa86e3392f0a70ad657a8e011622781d46214bd
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-01 07:56:43 +0600

    BE: add url to manage student academic-records

--- emis_backend/academy/urls.py
+++ emis_backend/academy/urls.py
@@ -34,6 +34,7 @@ from .views import (
     StudentsInCourseOfferView,
     MarksheetListByCourseOffer,
     CourseOfferCommentsView,
+    AcademicRecordsForStaff,
 )
 
 router = DefaultRouter()
@@ -78,6 +79,8 @@ urlpatterns = [
     path('course_offer/<int:course_offer_id>/students/', StudentsInCourseOfferView.as_view(), name='students_in_course_offer'),
     path('course-offer/marksheets/<int:course_offer_id>/', MarksheetListByCourseOffer.as_view(), name='marksheet-list-by-course-offer'),
     path('courseoffer/<int:course_offer_id>/comments/', CourseOfferCommentsView.as_view(), name='course_offer_discussion_comment'),
+    path('students/<int:student_id>/academic-records/', AcademicRecordsForStaff.as_view(), name='student_academic_records_for_satff'),
+    path('students/<int:student_id>/academic-records/<int:pk>/', AcademicRecordsForStaff.as_view(), name='student_academic_record_for_satff'),
     path('', include(router.urls)),
 ]
 
