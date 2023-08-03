06567c1e14974e58f80a4e702dcb9be4e40d9b6e
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-03 15:23:53 +0600

    BE: feat: define model UserCalendarActivity

--- emis_backend/academic_calendar/models.py
+++ emis_backend/academic_calendar/models.py
@@ -19,3 +19,14 @@ class DefaultCalendarActivity(models.Model):
         super(DefaultCalendarActivity, self).save(*args, **kwargs)
 
 
+
+class UserCalendarActivity(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
