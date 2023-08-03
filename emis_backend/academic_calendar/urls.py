c580f294de29150fcf7cfd62c39047103a57bf78
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-03 22:49:37 +0600

    BE: update: modify urls

--- emis_backend/academic_calendar/urls.py
+++ emis_backend/academic_calendar/urls.py
@@ -7,8 +7,8 @@ from rest_framework.routers import DefaultRouter
 from .views import DefaultCalendarActivityViewSet, UserCalendarActivityViewSet
 
 router = DefaultRouter()
-router.register(r'calendar-activity', DefaultCalendarActivityViewSet)
-router.register(r'user-calendar-activity', UserCalendarActivityViewSet)
+router.register(r'academic-activity', DefaultCalendarActivityViewSet)
+router.register(r'user-activity', UserCalendarActivityViewSet)
 
 
 urlpatterns = [
