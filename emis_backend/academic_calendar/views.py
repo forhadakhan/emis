f5ca32e7896a679a7eccde7a5dd2bda00e807dfd
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-03 15:39:24 +0600

    BE: update: add permission_classes in views

--- emis_backend/academic_calendar/views.py
+++ emis_backend/academic_calendar/views.py
@@ -1,18 +1,22 @@
 # academic_calendar/views.py
 
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet 
+from authentication.permissions import IsAdministratorOrStaff 
+from rest_framework.permissions import IsAuthenticated 
 from .models import DefaultCalendarActivity, UserCalendarActivity
 from .serializers import DefaultCalendarActivitySerializer, UserCalendarActivitySerializer
 
 
 
 class DefaultCalendarActivityViewSet(ModelViewSet):
+    permission_classes = [IsAdministratorOrStaff]
     queryset = DefaultCalendarActivity.objects.all()
     serializer_class = DefaultCalendarActivitySerializer
 
 
 
 class UserCalendarActivityViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = UserCalendarActivity.objects.all()
     serializer_class = UserCalendarActivitySerializer
 
