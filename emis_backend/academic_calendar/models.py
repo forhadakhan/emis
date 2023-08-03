d8278f69704da320e84aa4299becff6b14cbb68d
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-03 15:23:32 +0600

    BE: feat: define model DefaultCalendarActivity

--- emis_backend/academic_calendar/models.py
+++ emis_backend/academic_calendar/models.py
@@ -1,3 +1,21 @@
+# academic_calendar/models.py
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class DefaultCalendarActivity(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    date = models.DateField()
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.status:
+            self.status = 'REGULAR'
+        super(DefaultCalendarActivity, self).save(*args, **kwargs)
+
+
