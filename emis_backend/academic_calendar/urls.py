793ffcc948c25c43561ab5c3f1295a1701acb34c
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-03 23:04:59 +0600

    BE: add url (router) for 'weekends'

--- emis_backend/academic_calendar/urls.py
+++ emis_backend/academic_calendar/urls.py
@@ -4,11 +4,12 @@ app_name = 'academic_calendar'
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import DefaultCalendarActivityViewSet, UserCalendarActivityViewSet
+from .views import DefaultCalendarActivityViewSet, UserCalendarActivityViewSet, WeekendViewSet
 
 router = DefaultRouter()
 router.register(r'academic-activity', DefaultCalendarActivityViewSet)
 router.register(r'user-activity', UserCalendarActivityViewSet)
+router.register(r'weekends', WeekendViewSet)
 
 
 urlpatterns = [
