2a602bda58891c42f2b5b8a76d95635c287a6f83
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-03 16:11:46 +0600

    BE: update: define create_user_activity auto to add user

--- emis_backend/academic_calendar/views.py
+++ emis_backend/academic_calendar/views.py
@@ -20,4 +20,21 @@ class UserCalendarActivityViewSet(ModelViewSet):
     queryset = UserCalendarActivity.objects.all()
     serializer_class = UserCalendarActivitySerializer
 
+    @action(detail=False, methods=['post'])
+    def create_user_activity(self, request):
+        """
+        Custom action to create a new UserCalendarActivity associated with the authenticated user.
+
+        :param request: The HTTP request object.
+        :return: Response with the created UserCalendarActivity data or validation errors.
+        """
+        user = request.user  # Get the authenticated user from the request
+        data = request.data
+        data['user'] = user.pk  # Set the 'user' field with the authenticated user's ID
+        serializer = UserCalendarActivitySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
 
