7abe33a9a4d9dbd7ece4fbce12d52b71fc2cee08
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-03 15:34:04 +0600

    BE: feat: define UserCalendarActivityViewSet

--- emis_backend/academic_calendar/views.py
+++ emis_backend/academic_calendar/views.py
@@ -2,7 +2,7 @@
 
 from rest_framework.viewsets import ModelViewSet
 from .models import DefaultCalendarActivity, UserCalendarActivity
-from .serializers import DefaultCalendarActivitySerializer
+from .serializers import DefaultCalendarActivitySerializer, UserCalendarActivitySerializer
 
 
 
@@ -10,5 +10,10 @@ class DefaultCalendarActivityViewSet(ModelViewSet):
     queryset = DefaultCalendarActivity.objects.all()
     serializer_class = DefaultCalendarActivitySerializer
 
+
+
+class UserCalendarActivityViewSet(ModelViewSet):
+    queryset = UserCalendarActivity.objects.all()
+    serializer_class = UserCalendarActivitySerializer
 
 
