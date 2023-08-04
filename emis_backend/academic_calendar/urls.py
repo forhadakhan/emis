c5147c73e34ea40951d3f9d635bdfd7379dd55e7
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-04 16:34:57 +0600

    BE: chore: add example urls in comments

--- emis_backend/academic_calendar/urls.py
+++ emis_backend/academic_calendar/urls.py
@@ -12,6 +12,8 @@ router.register(r'academic-activity', DefaultCalendarActivityViewSet)
 """
 List view ____________________: .../academic-activity/
 Detail view __________________: .../academic-activity/<pk>/
+Delete view __________________: .../academic-activity/<pk>/
+Update (put/patch) view ______: .../academic-activity/<pk>/
 Filter by date - list view ___: .../academic-activity/?date=YYYY-MM-DD
 Filter by year - list view ___: .../academic-activity/?year=YYYY
 Filter by month - list view __: .../academic-activity/?year=2023&month=8
@@ -21,12 +23,25 @@ router.register(r'user-activity', UserCalendarActivityViewSet)
 """
 List view ____________________: .../user-activity/
 Detail view __________________: .../user-activity/<pk>/
+Delete view __________________: .../user-activity/<pk>/
+Update (put/patch) view ______: .../user-activity/<pk>/
 Filter by date - list view ___: .../user-activity/?date=YYYY-MM-DD
 Filter by year - list view ___: .../user-activity/?year=YYYY
 Filter by month - list view __: .../user-activity/?year=2023&month=8
 """
 
 router.register(r'weekends', WeekendViewSet)
+"""
+List view ____________________: .../weekends/
+Detail view __________________: .../weekends/<pk>/
+Delete view __________________: .../weekends/<pk>/
+Update (put/patch) view ______: .../weekends/<pk>/
+Filter by weekends only ______: .../weekends/?status=True
+Filter by weekends only ______: .../weekends/?status=1
+Filter by regular days _______: .../weekends/?status=False
+Filter by regular days _______: .../weekends/?status=0
+Filter by day name ___________: .../weekends/?day=Monday
+"""
 
 
 
