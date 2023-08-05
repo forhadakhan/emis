a85805514a81485fa069cbbfa65db92c2703a1c0
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-05 22:44:14 +0600

    BE: feat: define MediaSerializer

---
+++ emis_backend/press/serializers.py
@@ -0,0 +1,10 @@
+# press/serializers.py
+
+from rest_framework import serializers
+from .models import Media
+
+
+class MediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Media
+        fields = '__all__'
