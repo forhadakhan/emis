eb3e9df4deb9a89a44e1735b7df40136b5185dd5
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-05 22:47:40 +0600

    BE: feat: define press url for retrieve, update and delete

--- emis_backend/press/urls.py
+++ emis_backend/press/urls.py
@@ -3,13 +3,18 @@
 app_name = 'press'
 
 from django.urls import path
-from .views import MediaListCreateView
+from .views import MediaListCreateView, MediaRetrieveUpdateDeleteView
 
 
 urlpatterns = [
     path('', MediaListCreateView.as_view(), name='press-list-create'),
     # GET    ----- list:   api/press/
     # POST   --- create:   api/press/
+    
+    path('<int:pk>/', MediaRetrieveUpdateDeleteView.as_view(), name='press-retrieve-update-delete'),
+    # GET    - retrieve:   api/press/<int:pk>/
+    # PUT    --- update:   api/press/<int:pk>/
+    # DELETE --- update:   api/press/<int:pk>/
     
 ]
 
