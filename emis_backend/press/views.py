87b1eb2b2359879cb553bec9130f6e9d478597af
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-05 22:44:57 +0600

    BE: feat: define api view - MediaListCreateView

--- emis_backend/press/views.py
+++ emis_backend/press/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# press/views.py
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+from .models import Media
+from .serializers import MediaSerializer
+
+
+
+# Custom API view for listing and creating media content
+class MediaListCreateView(APIView):
+    # Handle GET request to retrieve a list of all media content
+    def get(self, request):
+        media = Media.objects.all()
+        serializer = MediaSerializer(media, many=True)
+        return Response(serializer.data)
+
+    # Handle POST request to create new media content
+    def post(self, request):
+        serializer = MediaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
