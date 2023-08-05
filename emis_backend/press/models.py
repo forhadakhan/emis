62c9b838e637214ab7242736fbbf104f96944bdd
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-05 22:43:48 +0600

    BE: feat: define model Media

--- emis_backend/press/models.py
+++ emis_backend/press/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+# press/models.py
 
-# Create your models here.
+from django.db import models
+from authentication.models import User
+
+
+class Media(models.Model):
+    CATEGORY_CHOICES = [
+        ('notice', 'Notice'),
+        ('news', 'News'),
+        ('announcement', 'Announcement'),
+        ('other', 'Other'),
+    ]
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=20, default='other')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
