ed7205671cc6d2ca65180c04856411da020a27c7
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-06 12:14:22 +0600

    BE: feat: implement MediaNestedSerializer

--- emis_backend/press/serializers.py
+++ emis_backend/press/serializers.py
@@ -1,10 +1,19 @@
 # press/serializers.py
 
 from rest_framework import serializers
+from authentication.serializers import UserBriefSerializer
 from .models import Media
 
 
 class MediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Media
+        fields = '__all__'
+
+
+class MediaNestedSerializer(serializers.ModelSerializer):
+    author = UserBriefSerializer()
+    
     class Meta:
         model = Media
         fields = '__all__'
