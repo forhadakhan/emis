ee70920dece61b3210bfa393236cf2f5861726bf
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-07 06:25:11 +0600

    BE: feat: include doc urls

--- emis_backend/emis_backend/urls.py
+++ emis_backend/emis_backend/urls.py
@@ -2,6 +2,24 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="EMIS API",
+      default_version='v1',
+      description="",
+      terms_of_service="https://www.emis.test/policies/terms/",
+      contact=openapi.Contact(email="contact@emis.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
@@ -20,5 +38,9 @@ urlpatterns = [
     path('api/', include('comments.urls', namespace='comments')),
     path('api/calendar/', include('academic_calendar.urls', namespace='academic_calendar')),
     path('api/press/', include('press.urls', namespace='press')),
+    path('doc/swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('doc/swagger.yaml/', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
+    path('doc/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('doc/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
