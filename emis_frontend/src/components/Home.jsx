65bfd92e098251a188f0ebfeb7b14658fdfad79e
FK <67508944+forhadakhan@users.noreply.github.com>
2023-08-31 02:54:41 +0600

    FE: feat: list posts in home / welcome page

--- emis_frontend/src/components/Home.jsx
+++ emis_frontend/src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+
 import '../styles/calendar.css';
 import CalendarComponent from './calendar/Calendar';
+
+import ListPosts from './media/ListPosts'
 
 
 const HomeComponent = ({ componentController }) => {
@@ -9,6 +12,12 @@ const HomeComponent = ({ componentController }) => {
             <h2 className="text-center h-4 pb-4 text-darkblue border-bottom border-darkblue"><i className="bi bi-house-door-fill"></i></h2>
             
             <CalendarComponent componentController={componentController} />
+
+            <div className="container p-3">
+            <div className="text-darkblue p-2">
+                <ListPosts />
+            </div>  
+            </div>  
             
         </div>
     );
